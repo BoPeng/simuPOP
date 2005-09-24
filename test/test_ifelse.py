e93694e32ea62be6e7e70abede182e100144d390
bpeng <bpeng@4f9f4801-c785-45ef-b63d-f11ee613d617>
2005-09-24 01:42:52 +0000

    change test_ifElse.py to unittest format

--- test/test_ifelse.py
+++ test/test_ifelse.py
@@ -1,36 +1,47 @@
-# test ifElse operator
+#!/usr/bin/env python
+#
+# This is a unittest file for ifElse operator
+#
+# Bo Peng (bpeng@rice.edu)
+# 
+# Last Modified: Sep, 2005
+# 
 
-from simuUtil import *
+from simuPOP import *
+from simuRPy import *
+import unittest, time
 
-simu = simulator(population(10, loci=[2]), randomMating())
+class TestIfElseOperator(unittest.TestCase):
+   
+  def setUp(self):
+    ' set up a simulator'
+    self.simu = simulator(population(100, loci=[2]), 
+      randomMating())
+  
+  def testIfElseOperator(self):
+    # now if we want to inject a mutation whenever fixation happens
+    self.simu.evolve(
+      preOps = [ initByFreq([.5,.5]) ],
+      ops = [
+        # count number of allels at this locus
+        stat(alleleFreq=[0]),
+        # inject 50% of allele 2 if this allele get lost
+        ifElse('alleleFreq[0][1]==1.', kamMutator(rate=.5, maxAllele=2, atLoci=[0]) ),
+        # the other way around?
+        ifElse('alleleFreq[0][1]==0.', kamMutator(rate=.5, maxAllele=2, atLoci=[0]) ),
+        # print allele frequencies, we should see patterns of reaching zero
+        # jump to .5
+        # pyEval(r'"%.2f\n" % alleleFreq[0][1]')
+        varPlotter('alleleFreq[0][1]', update=100, ylim=[0,1],
+          title='allele freq adjust to .5 when one allele get lost')
+      ],
+      end=1000
+    )
+    # hold the image for 10 seconds, 
+    time.sleep(10)
+    
+if __name__ == '__main__':
+  unittest.main()
+   
 
-simu.evolve(
-  preOps = [ initByFreq([.2,.8]) ],
-  ops = [
-    # count number of allels at this locus
-    stat(numOfAlleles=[0]),
-    # print it
-    pyEval('numOfAlleles'),
-    endl(),
-    # terminate if fixation happend
-    terminateIf('numOfAlleles[0]==1')
-  ],
-  end=100,
-  dryrun=False
-)
 
-# now if we want to inject a mutation whenever fixation happens
-simu.setGen(0)
-simu.evolve(
-  preOps = [ initByFreq([.2,.8]) ],
-  ops = [
-    # count number of allels at this locus
-    stat(numOfAlleles=[0]),
-    # print it
-    pyEval('numOfAlleles'),
-    endl(),
-    ifElse('numOfAlleles[0]==1', kamMutator(rate=.01, maxAllele=2, atLoci=[0]))
-  ],
-  end=100,
-  dryrun=False
-)
