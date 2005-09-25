bcd6a7f5e8e5a466b3cafbb8cb79eedf3db024a6
bpeng <bpeng@4f9f4801-c785-45ef-b63d-f11ee613d617>
2005-09-25 22:36:58 +0000

    fix the genertion of simuPOP_doc.i, add param parameter to pyOpeartor.

--- tools/build_doc.sh
+++ tools/build_doc.sh
@@ -1,14 +1,9 @@
 #!/bin/csh
 
-# make src and binary distribution on all platforms 
-# (currently mac, linux and solaris)
-#
 # This is part of simuPOP
 # 
 # Bo Peng (bpeng@rice.edu)
 #
-
-# first, make distribution:
 
 # make docstring
 
@@ -22,11 +17,6 @@ python doxy2swig.py /var/www/html/simuPOP_doc/doc/xml/index.xml tmp.i
 perl processDocString.pl tmp.i > ../src/simuPOP_doc.i
 
 # 3. post manual
-cd ../doc
-postGuide.sh 
+#cd ../doc
+#postGuide.sh 
  
-  
-# 4. compile library (regernerate .py file)
-cd ../src
-rm -f simuPOP_wrap.cpp 
-make -f Makefile.old simuPOP_wrap.cpp
