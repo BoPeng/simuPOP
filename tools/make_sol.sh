7b348935ef889bc5dd681ac4eb67230478b995e3
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2006-08-28 03:03:17 +0000

    let setup.py detect swig version, and some make_xx.sh changes

--- tools/make_sol.sh
+++ tools/make_sol.sh
@@ -11,11 +11,11 @@
 # Note: use local gsl copy to build binary distribution
 #
 VER=`cat /var/www/html/simuPOP/download/latestversion`
-ssh thor.stat.rice.edu '/bin/rm -rf temp &&  mkdir temp'
-scp /var/www/html/simuPOP/download/simuPOP-$VER-src.tar.gz thor:temp
+ssh cedric '/bin/rm -rf temp &&  mkdir temp'
+scp /var/www/html/simuPOP/download/simuPOP-$VER-src.tar.gz cedric:temp
 
-UNCOMPRESS="/usr/local/bin/tar zxf simuPOP-$VER-src.tar.gz"
-BUILD="setenv CXX /usr/site/gcc-3.3/bin/g++; setenv CC /usr/site/gcc-3.3/bin/gcc; python setup.py bdist_dumb"
-ssh -X thor.stat.rice.edu "cd temp && $UNCOMPRESS && cd simuPOP-$VER && $BUILD"
-scp thor:temp/simuPOP-$VER/dist/simuPOP-$VER*.tar.gz /var/www/html/simuPOP/download/simuPOP-$VER-sol-py23.tar.gz
+UNCOMPRESS="gunzip -d simuPOP-$VER-src.tar.gz; tar xf simuPOP-$VER-src.tar"
+BUILD="python setup.py bdist_dumb"
+ssh -X cedric "cd temp && $UNCOMPRESS && cd simuPOP-$VER && $BUILD"
+scp cedric:temp/simuPOP-$VER/dist/simuPOP-$VER*.tar.gz /var/www/html/simuPOP/download/simuPOP-$VER-sol-py23.tar.gz
 
