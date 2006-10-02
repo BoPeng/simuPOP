0799f4874db1f2f90d70d0f2f9d4d6b406903603
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2006-10-02 04:24:36 +0000

    automatic checkin on Sun Oct  1 23:12:43 2006

--- trunk/tools/make_sol.sh
+++ trunk/tools/make_sol.sh
@@ -11,11 +11,11 @@
 # Note: use local gsl copy to build binary distribution
 #
 VER=`cat /var/www/html/simuPOP/download/latestversion`
-ssh cedric '/bin/rm -rf temp &&  mkdir temp'
-scp /var/www/html/simuPOP/download/simuPOP-$VER-src.tar.gz cedric:temp
+ssh mdapenrose '/bin/rm -rf temp &&  mkdir temp'
+scp /var/www/html/simuPOP/download/simuPOP-$VER-src.tar.gz mdapenrose:temp
 
 UNCOMPRESS="gunzip -d simuPOP-$VER-src.tar.gz; tar xf simuPOP-$VER-src.tar"
 BUILD="python setup.py bdist_dumb"
-ssh -X cedric "cd temp && $UNCOMPRESS && cd simuPOP-$VER && $BUILD"
-scp cedric:temp/simuPOP-$VER/dist/simuPOP-$VER*.tar.gz /var/www/html/simuPOP/download/simuPOP-$VER-sol-py23.tar.gz
+ssh -X mdapenrose "cd temp && $UNCOMPRESS && cd simuPOP-$VER && $BUILD"
+scp mdapenrose:temp/simuPOP-$VER/dist/simuPOP-$VER*.tar.gz /var/www/html/simuPOP/download/simuPOP-$VER-sol-py23.tar.gz
 
