8b1d64dd3ceb3535b177db8ef304ac7a777c3325
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2006-12-31 23:37:47 +0000

    remove postGuide.sh

--- trunk/doc/postGuideSF.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-scp  /var/www/html/simuPOP_doc/* simupop@shell.sf.net:/home/groups/s/si/simupop/htdocs/simuPOP_doc
-scp -r  /var/www/html/simuPOP_doc/userGuide simupop@shell.sf.net:/home/groups/s/si/simupop/htdocs/simuPOP_doc
-scp -r /var/www/html/simuPOP_doc/refManual simupop@shell.sf.net:/home/groups/s/si/simupop/htdocs/simuPOP_doc/regManual
