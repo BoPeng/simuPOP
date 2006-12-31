ce0e73e22eafe3f9b798b200e5de2a451a21a6d1
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2006-12-31 04:43:54 +0000

    replace make_linux.sh with build_linux.py

---
+++ tools/build_linux.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os, sys, shutil
+from snapshot import *
+
+def build_linux():
+    ver = open(last_version_file).read()
+    #
+    if not os.path.isfile('%s/simuPOP-%s.zip' % (download_directory, ver)):
+        print 'Source package does not exist. Please run build_src.py first'
+        sys.exit(1)
+    # 
+    # build
+    os.system('/bin/rm -rf simuPOP-%s' % ver)
+    os.system('tar zxf simuPOP-%s-src.tar.gz' % ver)
+    os.chdir('simuPOP-%s' % ver)
+    os.system('python setup.py bdist --formats=gztar,rpm')
+    # coppy files
+    shutil.copy('dist/simuPOP-%s-1.x86_64.rpm' % ver, '%s/simuPOP-%s-1.x86_64.rpm' % (download_directory, ver))
+    shutil.copy('dist/simuPOP-%s-1.src.rpm' % ver, '%s/simuPOP-%s-1.src.rpm' % (download_directory, ver))
+
+if __name__ == '__main__':
+    os.chdir(user_tmp_directory)
+    build_linux()
