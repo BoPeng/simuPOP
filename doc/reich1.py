586f50e56570a9f5d36a32ba3ecad67695742b2c
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2007-01-22 17:07:55 +0000

    fix typos in reich*.py

--- doc/reich1.py
+++ doc/reich1.py
@@ -9,11 +9,11 @@ def ins_exp(gen, oldSize=[]):
     else:
         return [finalSize]
 
-def simulate(incSenario):
+def simulate(incScenario):
     simu = simulator(                                        # create a simulator
-        population(subPop=incSenario(0), loci=[1,1],
+        population(subPop=incScenario(0), loci=[1,1],
             infoFields=['fitness']),                         # inital population
-        randomMating(newSubPopSizeFunc=incSenario)           # random mating
+        randomMating(newSubPopSizeFunc=incScenario)           # random mating
     )
 
 simulate(ins_exp)
