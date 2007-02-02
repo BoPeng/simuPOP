40570915421027091755faca61badc2d535efb68
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2007-02-02 06:30:15 +0000

    build.py

--- trunk/tools/build.py
+++ trunk/tools/build.py
@@ -70,6 +70,8 @@ def setVersionRevision(release):
         rev = cmdOutput('svnversion .')
         if ':' in rev:
             rev = rev.split(':')[0]
+    else:
+        rev = 9999
     # write last_revision_file and last_version_file
     file = open(last_revision_file, 'w')
     file.write(rev)
