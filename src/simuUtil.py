a4241cba3db4a18ac64df4b43fc5a72f7e6a1473
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2007-02-27 03:57:57 +0000

    allow output of case control data in QTDT format

--- src/simuUtil.py
+++ src/simuUtil.py
@@ -1312,34 +1312,39 @@ def SaveQTDT(pop, output='', outputExpr='', loci=[], affectionCode=['U', 'A'],
     # number of pedigrees
     # get unique pedgree id numbers
     from sets import Set
-    peds = Set(pop.indInfo('pedindex', False))
-    # do not count peds
-    peds.discard(-1)
-    #
-    newPedIdx = 1
-    #
-    for ped in peds:
-        id = 1
-        # -1 means no parents
-        pastmap = {-1:0}
-        # go from generation 2, 1, 0 (for example)
-        for anc in range(pop.ancestralDepth(), -1, -1):
-            newmap = {-1:0}
-            pop.useAncestralPop(anc)
-            # find all individual in this pedigree
-            for i in range(pop.popSize()):
-                ind = pop.individual(i)
-                if ind.info('pedindex') == ped:
-                    dad = int(ind.info('father_idx'))
-                    mom = int(ind.info('mother_idx'))
-                    if dad == mom and dad != -1:
-                        print "Something wrong with pedigree %d, father and mother idx are the same: %s" % \
-                            (ped, dad)
-                    writeInd(ind, newPedIdx, id, pastmap.setdefault(dad, 0), pastmap.setdefault(mom, 0))
-                    newmap[i] = id
-                    id += 1
-            pastmap = newmap
-        newPedIdx += 1
+    if pop.hasInfoField('pedindex') and pop.ancestralDepth() >= 1:
+        peds = Set(pop.indInfo('pedindex', False))
+        # do not count peds
+        peds.discard(-1)
+        #
+        newPedIdx = 1
+        #
+        for ped in peds:
+            id = 1
+            # -1 means no parents
+            pastmap = {-1:0}
+            # go from generation 2, 1, 0 (for example)
+            for anc in range(pop.ancestralDepth(), -1, -1):
+                newmap = {-1:0}
+                pop.useAncestralPop(anc)
+                # find all individual in this pedigree
+                for i in range(pop.popSize()):
+                    ind = pop.individual(i)
+                    if ind.info('pedindex') == ped:
+                        dad = int(ind.info('father_idx'))
+                        mom = int(ind.info('mother_idx'))
+                        if dad == mom and dad != -1:
+                            print "Something wrong with pedigree %d, father and mother idx are the same: %s" % \
+                                (ped, dad)
+                        writeInd(ind, newPedIdx, id, pastmap.setdefault(dad, 0), pastmap.setdefault(mom, 0))
+                        newmap[i] = id
+                        id += 1
+                pastmap = newmap
+            newPedIdx += 1
+    else:
+        # rare case: no pedigree structure, only output the last generation without parents
+        for idx, ind in enumerate(pop.individuals()):
+            writeInd(ind, idx, 1, 0, 0)
     pedOut.close()
 
 
