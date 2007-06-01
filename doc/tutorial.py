089dbc3028dabd8a07ab568a562d572eab87167f
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2007-06-01 05:22:12 +0000

    more slides

--- doc/tutorial.py
+++ doc/tutorial.py
@@ -51,3 +51,42 @@ simu.evolve(
 #PS epstopdf ld80.eps
 #PS epstopdf ld90.eps
 #PS epstopdf ld100.eps
+#file log/tutorial_population.log
+pop = population(size=10, loci=[2, 3])
+Dump(pop)
+#end
+#file log/tutorial_genostru.log
+pop = population(subPop=[200, 300], loci=[3, 2], 
+    maxAllele=3, ploidy=3, 
+    lociPos=[[1, 3, 5], [2.5, 4]], 
+    alleleNames=['A', 'C', 'T', 'G'])
+pop.numLoci(0)
+pop.totNumLoci()
+pop.locusPos(4)
+pop.subPopSize(1)
+pop.popSize()
+pop.ploidyName()
+pop.individual(1).allele(1, 2)
+#end
+#file log/tutorial_pop_manipulate.log
+# make a copy of pop
+pop1 = pop.clone()
+# remove loci 2, 3, 4
+pop.removeLoci(keep=[0, 1])
+# pop2 will have 3 chromosomes, with loci 2, 3, 2
+pop2 = MergePopulationsByLoci(pops=[pop, pop1])
+# randomly assign alleles using given allele frequencies
+InitByFreq(pop2, [0.8, .2])
+# calculate population allele frequency
+Stat(pop2, alleleFreq=range(pop2.totNumLoci()))
+# print allele frequency
+print pop2.dvars().alleleFreq
+# assign affection status using a penetrance model
+MapPenetrance(pop2, locus=1, 
+    penetrance={'0-0': 0.05, '0-1': 0.2, '1-1': 0.8})
+# draw case control sample
+(sample,) = CaseControlSample(pop2, cases=5, controls=5)
+# have a look at the sample
+Dump(sample, infoOnly=True)
+Dump(sample, alleleOnly=True)
+#end
