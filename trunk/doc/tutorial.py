918c7455163d9a0007793e8b3cbcc2143314a0d6
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2007-06-08 22:27:31 +0000

    update tutorials

--- trunk/doc/tutorial.py
+++ trunk/doc/tutorial.py
@@ -53,158 +53,12 @@ simu.evolve(
 # #PS epstopdf ld80.eps
 # #PS epstopdf ld90.eps
 # #PS epstopdf ld100.eps
-#file log/tutorial_population.log
-pop = population(size=10, loci=[2, 3])
-Dump(pop)
-#end
-#file log/tutorial_genostru.log
-pop = population(subPop=[200, 300], loci=[3, 2], 
-    maxAllele=3, ploidy=3, 
-    lociPos=[[1, 3, 5], [2.5, 4]], 
-    alleleNames=['A', 'C', 'T', 'G'])
-pop.numLoci(0)
-pop.totNumLoci()
-pop.locusPos(4)
-pop.subPopSize(1)
-pop.popSize()
-pop.ploidyName()
-pop.individual(1).allele(1, 2)
-#end
-#file log/tutorial_pop_manipulate.log
-# make a copy of pop
-pop1 = pop.clone()
-# remove loci 2, 3, 4
-pop.removeLoci(keep=[0, 1])
-# pop2 will have 3 chromosomes, with loci 2, 3, 2
-pop2 = MergePopulationsByLoci(pops=[pop, pop1])
-# randomly assign alleles using given allele frequencies
-InitByFreq(pop2, [0.8, .2])
-# calculate population allele frequency
-Stat(pop2, alleleFreq=range(pop2.totNumLoci()))
-# print allele frequency
-print pop2.dvars().alleleFreq
-# assign affection status using a penetrance model
-MapPenetrance(pop2, locus=1, 
-    penetrance={'0-0': 0.05, '0-1': 0.2, '1-1': 0.8})
-# draw case control sample
-(sample,) = CaseControlSample(pop2, cases=5, controls=5)
-# save sample in Merlin QTDT format
-from simuUtil import SaveQTDT
-SaveQTDT(sample, output='sample', affectionCode=['U', 'A'], 
-    fields=['affection'])
-# have a look at the sample in Merlin-QTDT Format
-print open('sample.map').read()
-print open('sample.dat').read()
-print open('sample.ped').read()
-#end
-#file log/tutorial_pop_variable.log
-pop = population(subPop=[5, 10], loci=[5])
-InitByFreq(pop, [.6, .3, .1])
-Stat(pop, alleleFreq=[1], genoFreq=[2])
-print pop.dvars().alleleFreq[1][0]
-from simuUtil import ListVars
-ListVars(pop.dvars(), useWxPython=False)
-#end
-#file log/tutorial_individual.log
-pop = population(subPop=[5, 8], loci=[5], 
-    infoFields=['penetrance'])
-InitByFreq(pop, [.6, .3, .1])
-MaPenetrance(pop, locus=2, penetrance=[0.05, 0.2, 0.5],
-    wildtype=[0], infoFields=['penetrance'])
-# iterate through all inviduals in subPop 1
-for ind in pop.individuals(1):
-    print 'Aff: %d Fit: %.3f Geno: %d %d' % \
-        (ind.affected(), ind.info('penetrance'), \
-        ind.allele(2, 0), ind.allele(2, 1))
-
-#end
-#file log/tutorial_info.log
-pop = population(100, loci=[5, 8],
-    infoFields=['father_idx', 'mother_idx'])
-simu = simulator(pop, randomMating(numOffspring=2))
-simu.evolve(ops=[parentsTagger()], end=5)
-ind = simu.population(0).individual(0)
-ind1 = simu.population(0).individual(1)
-print ind.info('father_idx'), ind.info('mother_idx')
-print ind1.info('father_idx'), ind1.info('mother_idx')
-
-#end
-#file log/tutorial_op_stage.log
-simu = simulator(
-    population(subPop=[20, 80], loci=[3]),
-    randomMating())
-simu.evolve(
-    preOps = [initByFreq([0.2, 0.8])],
-    ops = [
-        kamMutator(maxAllele=10, rate=0.00005, atLoci=[0,2]),
-        recombinator(rate=0.001),
-        dumper(stage=PrePostMating),
-        stat(alleleFreq=[1]),
-    ],
-    dryrun=True
-)
-#end
-#file log/tutorial_op_gen.log
-simu = simulator(
-    population(10000, loci=[3]),
-    randomMating())
-eval1 = r"'Gen: %3d  Freq: %f\n' % (gen, alleleFreq[1][0])"
-eval2 = r"'Last Gen: %3d  Freq: %s\n' % (gen, alleleFreq[1])"
-simu.evolve(
-    preOps = [initByFreq([0.3, 0.7])],
-    ops = [
-        recombinator(rate=0.01, begin=10, end=30),
-        stat(alleleFreq=[1], step=10),
-        pyEval(eval1, step=10),
-        pyEval(eval2, at=[-1])
-    ],
-    end = 50
-)
-#end
-#file log/tutorial_op_rep.log
-simu = simulator(
-    population(100, loci=[3]),
-    randomMating(), 
-    rep=5, grp=[1,1,2,2,2])
-simu.evolve(
-    preOps = [initByFreq([0.5, 0.5])],
-    ops = [
-        stat(alleleFreq=[1]),
-        recombinator(rate=0.01, grp=1),
-        recombinator(rate=0.01, grp=2),
-        pyEval(r"'%.2f ' % alleleFreq[1][0]", grp=1),
-        pyEval(r"'\n'", rep=REP_LAST),
-    ],
-    end=5
-)
-#end
-#file log/tutorial_op_output.log
-simu = simulator(
-    population(100, loci=[3]),
-    randomMating(), 
-    rep=5, grp=[1,1,2,2,2])
-simu.evolve(
-    preOps = [initByFreq([0.5, 0.5])],
-    ops = [
-        stat(alleleFreq=[1]),
-        pyEval(r"'%.2f ' % alleleFreq[1][0]", 
-            output='>>out'),
-        pyEval(r"'\n'", rep=REP_LAST, output='>>out'),
-        pyEval(r"'%.2f ' % alleleFreq[1][0]", 
-            outputExpr="'>>out%d' % grp"),
-    ],
-    end=2
-)
-print open('out').read()
-print open('out1').read()
-print open('out2').read()
-#end
-#file log/tutorial_mating_demo.log
+#file log/tutorial_popsize.log
 def lin_inc(gen, oldsize=[]):
     return [10+gen]*5
 
 simu = simulator(
-    population(subPop=lin_inc(1), loci=[1]),
+    population(subPop=[5]*5, loci=[1]),
     randomMating(newSubPopSizeFunc=lin_inc)
 )
 simu.evolve(
@@ -215,48 +69,80 @@ simu.evolve(
     end=5
 )
 #end
-#file log/tutorial_mating_offspring.log
+#file log/tutorial_stat.log
 simu = simulator(
-    population(size=10000, loci=[1]),
-    randomMating(),
+    population(subPop=[10000]*2, loci=[10]),
+    randomMating()
 )
 simu.evolve(
-    preOps = [initByFreq([0.1, 0.9])],
-    ops = [ ], end=100
+    preOps = [
+        initByFreq([0.2, 0.8], subPop=[0]),
+        initByFreq([0.8, 0.2], subPop=[1]),
+    ],
+    ops = [
+        stat(LD=[[0,1], [5,6]], Fst=range(10), step=100),
+        migrator(rate=[[0, 0.01], [0, 0.02]]),
+        pyEval(r'"Gen: %4d LD: %.3f R2: %.3f Fst: %.3f\n"' \
+            ' % (gen, LD[0][1], R2[0][1], AvgFst)', 
+            step=100)
+    ],
+    end=1000
 )
-simu.setMatingScheme(randomMating(numOffspring=2))
-simu.addInfoFields(['father_idx', 'mother_idx'])
-simu.setAncestralDepth(1)
-simu.step(ops=[parentsTagger()])
-pop = simu.getPopulation(0)
-MaPenetrance(pop, locus=0, penetrance=[0.05, 0.1, 0.5])
-AffectedSibpairSample(pop, size=100)
+#end
+#file log/tutorial_pyPenetrance.log
+def myPene(geno):
+    'geno is the genotype at the two given loci'
+    loc1 = geno[0] + geno[1]
+    loc2 = geno[2] + geno[3]
+    if (loc1 == 2 and loc2 < 2) or \
+        (loc1 < 2 and loc2 == 2):
+        return 0.1
+    else:
+        return 0.5
+
+pop = population(subPop=[1000], loci=[6])
+# initialize the population
+InitByFreq(pop, [0.1, 0.9])
+# apply penetrance and obtain affection status
+PyPenetrance(pop, loci=[3, 5], func=myPene)
+# draw case control sample
+(sample,) = CaseControlSample(pop, cases=3, controls=3)
+# save sample in Merlin QTDT format
+from simuUtil import SaveQTDT
+SaveQTDT(sample, output='sample', affectionCode=['U', 'A'], 
+    fields=['affection'])
+# have a look at the sample in Merlin-QTDT Format
+print open('sample.map').read()
+print open('sample.dat').read()
+print open('sample.ped').read()
+#end
+#file log/tutorial_eff_alleles.log
+def Ne(pop, loci):
+    'Calculate effective number of alleles'
+    Stat(pop, alleleFreq=loci)
+    pop.dvars().Ne = {}
+    v = pop.dvars().alleleFreq
+    for locus in loci:
+        f0 = 1 - v[locus][0]
+        Ne = 1./f0*f0/sum([x*x for x in v[locus][1:]])
+        pop.dvars().Ne[locus] = Ne
+    return True
+
+pop = population(1000, loci=[2,3])
+      
 #end
 #file log/tutorial_hapmap.log
 genes = [
-    ("p53exon4", "rs1042522"),
-    ("p53_6", "rs1625895"),
-    ("xpdex23", "rs1799793"),
-    ("xpdex10", "rs13181"),
-    ("xpa", "rs1800975"),
-    ("xpg1104", "rs17655"),
-    ("xpf662", "rs2020955"),
-    ("ercc61097", "rs2228526"),
-    ("ercc61230", "rs4253211"),
-    ("xpc_939", "rs2228001"),
-    ("ccnh", "rs2266690"),
-    ("rad23", "rs1805329"),
-    ("ercc1", "rs3212986"),
-    ("xpc_499", "rs2228000"), 
+    "rs1042522",
+    "rs1625895",
+    "rs1799793",
 ]
-
-names = [x[1] for x in genes]
 pops = []
 for i in range(1, 23):
     print "Loading hapmap chromosome %d..." % i
     pop = LoadPopulation('hapmap_%d.bin' % i)
     markers = []
-    for name in names:
+    for name in genes:
         try:
             idx = pop.locusByName(name)
             markers.append(idx)
