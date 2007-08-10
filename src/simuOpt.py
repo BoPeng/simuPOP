fa8befb9c8018586ed238b14149f9e79fa5d51ff
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2007-08-10 04:47:05 +0000

    add __doc__ to simuOpt.py

--- src/simuOpt.py
+++ src/simuOpt.py
@@ -23,7 +23,9 @@
 #        59 Temple Place - Suite 330, Boston, MA    02111-1307, USA.
 ############################################################################
 
-
+'''
+simuOpt documentation (FIXME)
+'''
 
 # First try to get environmental variable
 
