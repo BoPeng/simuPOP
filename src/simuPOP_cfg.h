4b13f1861f793622a6bcce3ec5382438929224e3
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2007-11-04 07:46:23 +0000

    adjust format for simuPOP_cfg.h

--- src/simuPOP_cfg.h
+++ src/simuPOP_cfg.h
@@ -1,30 +1,30 @@
 /***************************************************************************
- *   Copyright (C) 2004 by Bo Peng                                         *
- *   bpeng@rice.edu
- *                                                                         *
- *   $LastChangedDate$
- *   $Rev$                                                     *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+*   Copyright (C) 2004 by Bo Peng                                         *
+*   bpeng@rice.edu
+*                                                                         *
+*   $LastChangedDate$
+*   $Rev$                                                     *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
 
 /**
-\file
-\brief system config file (typedef and macros)
-*/
+ \file
+ \brief system config file (typedef and macros)
+ */
 
 /// global configuration
 #ifndef _SIMUPOP_CONFIG_H
@@ -42,22 +42,22 @@
 
 #ifdef _MSC_VER
 
-#define WORDBIT (8*sizeof(unsigned))
-#define WORDTYPE unsigned
-#define BITPTR(ref) ref._Myptr
-#define BITOFF(ref) ref._Myoff
+ #define WORDBIT (8 * sizeof(unsigned))
+ #define WORDTYPE unsigned
+ #define BITPTR(ref) ref._Myptr
+ #define BITOFF(ref) ref._Myoff
 
 #else
-#define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
-#if GCC_VERSION > 30400
-#define WORDBIT std::_S_word_bit
-#else
+ #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+ #if GCC_VERSION > 30400
+  #define WORDBIT std::_S_word_bit
+ #else
 // previous version uses _M_word_bit
-#define WORDBIT std::_M_word_bit
-#endif
-#define WORDTYPE std::_Bit_type
-#define BITPTR(ref) ref._M_p
-#define BITOFF(ref) ref._M_offset
+  #define WORDBIT std::_M_word_bit
+ #endif
+ #define WORDTYPE std::_Bit_type
+ #define BITPTR(ref) ref._M_p
+ #define BITOFF(ref) ref._M_offset
 #endif
 
 #include <string>
@@ -77,47 +77,47 @@ typedef unsigned int UINT;
 
 // for msvc, I have a portable stdint.h under win32 directory
 #ifdef HAVE_STDINT_H
-#include <stdint.h>
+ #include <stdint.h>
 #else
 // for solaris, I have to use inttypes.h because there is no stdint.h
-#include <inttypes.h>
+ #include <inttypes.h>
 #endif
 
 // NOTE: the change of allele type here may need similar changes
 // in the wrapper file simuPOP_common.i
 #ifdef LONGALLELE
-typedef uint16_t  Allele;
-typedef uint16_t& AlleleRef;
+typedef uint16_t Allele;
+typedef uint16_t & AlleleRef;
 
-#define AlleleInc(a)  ++(a)
-#define AlleleDec(a)  --(a)
-#define AlleleAdd(a, b) (a)+=(b)
-#define AlleleMinus(a, b) (a)-=(b)
-#define AlleleUnsigned(a) (a)
+ #define AlleleInc(a)  ++ (a)
+ #define AlleleDec(a)  -- (a)
+ #define AlleleAdd(a, b) (a) += (b)
+ #define AlleleMinus(a, b) (a) -= (b)
+ #define AlleleUnsigned(a) (a)
 
 #else
 
-#ifdef BINARYALLELE
+ #ifdef BINARYALLELE
 
 typedef bool Allele;
 typedef vector<Allele>::reference AlleleRef;
 // bool type, inc go to 1, dec go to 0
-#define AlleleInc(a)  (a)=true
-#define AlleleDec(a)  (a)=false
-#define AlleleAdd(a, b) (a)=((b)==0?(a):((b)>0?true:false))
-#define AlleleMinus(a, b) (a)=((b)==0?(a):((b)>0?false:true))
-#define AlleleUnsigned(a) (static_cast<unsigned char>(a))
+  #define AlleleInc(a)  (a) = true
+  #define AlleleDec(a)  (a) = false
+  #define AlleleAdd(a, b) (a) = ((b) == 0 ? (a) : ((b) > 0 ? true : false))
+  #define AlleleMinus(a, b) (a) = ((b) == 0 ? (a) : ((b) > 0 ? false : true))
+  #define AlleleUnsigned(a) (static_cast<unsigned char>(a))
 
-#else
+ #else
 
-typedef unsigned char  Allele;
-typedef unsigned char& AlleleRef;
-#define AlleleInc(a)  ++(a)
-#define AlleleDec(a)  --(a)
-#define AlleleAdd(a, b) (a)+=(b)
-#define AlleleMinus(a, b) (a)-=(b)
-#define AlleleUnsigned(a) (a)
-#endif
+typedef unsigned char Allele;
+typedef unsigned char & AlleleRef;
+  #define AlleleInc(a)  ++ (a)
+  #define AlleleDec(a)  -- (a)
+  #define AlleleAdd(a, b) (a) += (b)
+  #define AlleleMinus(a, b) (a) -= (b)
+  #define AlleleUnsigned(a) (a)
+ #endif
 #endif
 
 typedef std::vector<Allele>::iterator GenoIterator;
@@ -130,7 +130,7 @@ const unsigned long MaxRandomNumber = std::numeric_limits<int32_t>::max();
 #define PopSWIGType "simuPOP::population *"
 #define IndSWIGType "simuPOP::individual *"
 
-enum Sex{ Male = 1, Female = 2};
+enum Sex { Male = 1, Female = 2 };
 
 // info is usually used for subpopulation index.
 // signed short should be enough.
@@ -166,107 +166,127 @@ typedef std::map<int, double>              intDict;
 
 namespace simuPOP
 {
-	/// exception handler. Exceptions will be passed to Python.
-	class Exception
+/// exception handler. Exceptions will be passed to Python.
+class Exception
+{
+public:
+
+	/// constructor
+	/// \param msg error message
+	Exception(string msg) : m_msg(msg)
 	{
-		public:
+	}
 
-			/// constructor
-			/// \param msg error message
-			Exception(string msg):m_msg(msg){}
+	/// return error message
+	const char * message()
+	{
+		return m_msg.c_str();
+	}
 
-			/// return error message
-			const char* message(){ return m_msg.c_str(); }
-
-			virtual ~Exception(){};
-
-		private:
-			/// error message
-			string m_msg;
+	virtual ~Exception()
+	{
 	};
 
-	/// exception, thrown if out of memory
-	class StopIteration: public Exception
-	{
-		public:
-			StopIteration(string msg):Exception(msg){};
-	};
+private:
+	/// error message
+	string m_msg;
+};
 
-	/// exception, thrown if out of memory
-	class OutOfMemory: public Exception
+/// exception, thrown if out of memory
+class StopIteration : public Exception
+{
+public:
+	StopIteration(string msg) : Exception(msg)
 	{
-		public:
-			OutOfMemory(string msg):Exception(msg){};
 	};
+};
 
-	/// exception, thrown if file io failure
-	class IOError: public Exception
+/// exception, thrown if out of memory
+class OutOfMemory : public Exception
+{
+public:
+	OutOfMemory(string msg) : Exception(msg)
 	{
-		public:
-			IOError(string msg):Exception(msg){};
 	};
+};
 
-	/// exception, thrown if index out of range
-	class IndexError: public Exception
+/// exception, thrown if file io failure
+class IOError : public Exception
+{
+public:
+	IOError(string msg) : Exception(msg)
 	{
-		public:
-			IndexError(string msg):Exception(msg){};
 	};
+};
 
-	/// exception, thrown if type mismatch
-	class TypeError: public Exception
+/// exception, thrown if index out of range
+class IndexError : public Exception
+{
+public:
+	IndexError(string msg) : Exception(msg)
 	{
-		public:
-			TypeError(string msg):Exception(msg){};
 	};
+};
 
-	/// exception, thrown if value of range etc
-	class ValueError: public Exception
+/// exception, thrown if type mismatch
+class TypeError : public Exception
+{
+public:
+	TypeError(string msg) : Exception(msg)
 	{
-		public:
-			ValueError(string msg):Exception(msg){};
 	};
+};
 
-	/// exception, thrown if system error occurs
-	class SystemError: public Exception
+/// exception, thrown if value of range etc
+class ValueError : public Exception
+{
+public:
+	ValueError(string msg) : Exception(msg)
 	{
-		public:
-			SystemError(string msg):Exception(msg){};
 	};
+};
 
-	// define DEBUG codes
-	// DEbUG_CODE_LENGTH should be the number of debug codes
+/// exception, thrown if system error occurs
+class SystemError : public Exception
+{
+public:
+	SystemError(string msg) : Exception(msg)
+	{
+	};
+};
+
+// define DEBUG codes
+// DEbUG_CODE_LENGTH should be the number of debug codes
 #define DBG_CODE_LENGTH 21
 
-	enum DBG_CODE
-	{
-		DBG_ALL=0,
-		DBG_GENERAL=1,
-		DBG_UTILITY=2,
-		DBG_OPERATOR=3,
-		DBG_SIMULATOR=4,
-		DBG_INDIVIDUAL=5,
-		DBG_OUTPUTER=6,
-		DBG_MUTATOR=7,
-		DBG_RECOMBINATOR=8,
-		DBG_INITIALIZER=9,
-		DBG_POPULATION=10,
-		DBG_STATOR=11,
-		DBG_TERMINATOR=12,
-		DBG_TAGGER=13,
-		DBG_VISUALIZER=14,
-		DBG_SELECTOR=15,
-		DBG_MATING=16,
-		DBG_MIGRATOR=17,
-		DBG_PROFILE=18,
-		DBG_MPI=19,
-		DBG_DEVEL=20
-	};
+enum DBG_CODE {
+	DBG_ALL = 0,
+	DBG_GENERAL = 1,
+	DBG_UTILITY = 2,
+	DBG_OPERATOR = 3,
+	DBG_SIMULATOR = 4,
+	DBG_INDIVIDUAL = 5,
+	DBG_OUTPUTER = 6,
+	DBG_MUTATOR = 7,
+	DBG_RECOMBINATOR = 8,
+	DBG_INITIALIZER = 9,
+	DBG_POPULATION = 10,
+	DBG_STATOR = 11,
+	DBG_TERMINATOR = 12,
+	DBG_TAGGER = 13,
+	DBG_VISUALIZER = 14,
+	DBG_SELECTOR = 15,
+	DBG_MATING = 16,
+	DBG_MIGRATOR = 17,
+	DBG_PROFILE = 18,
+	DBG_MPI = 19,
+	DBG_DEVEL = 20
+};
 
-	// DBG_NAMES are defined in utility.cpp
-	// if you add any debug code here, you need to add its name
-	// in utility.cpp as well. Otherwise, turnOnDebug, turnOffDebug
-	// etc will be function well.
+// DBG_NAMES are defined in utility.cpp
+// if you add any debug code here, you need to add its name
+// in utility.cpp as well. Otherwise, turnOnDebug, turnOffDebug
+// etc will be function well.
 
 #define REP_ALL             -2
 #define REP_CUR             -2
@@ -275,60 +295,60 @@ namespace simuPOP
 #define PreMating            1
 #define DuringMating         2
 #define PostMating           4
-	// combinations of mating scheme.
-#define PrePostMating        (PreMating+PostMating)
-#define PreDuringMating      (PreMating+DuringMating)
-#define DuringPostMating     (DuringMating+PostMating)
-#define PreDuringPostMating  (PreMating+DuringMating+PostMating)
+// combinations of mating scheme.
+#define PrePostMating        (PreMating + PostMating)
+#define PreDuringMating      (PreMating + DuringMating)
+#define DuringPostMating     (DuringMating + PostMating)
+#define PreDuringPostMating  (PreMating + DuringMating + PostMating)
 
-	// used to set local variables
+// used to set local variables
 #define subPopVar_String(sp, var) (string("subPop[") + toStr(sp) + "]{\'" + var + "\'}")
 
-	// standard library
+// standard library
 #ifndef OPTIMIZED
 
-#define DBG_ASSERT(cond, exception, message) \
-if(!(cond)) \
-{ \
-	throw exception( \
-	toStr(__FILE__)+toStr(":")+toStr(__LINE__)+toStr(" ")+message); \
-}
+ #define DBG_ASSERT(cond, exception, message) \
+	if (!(cond)) \
+	{ \
+		throw exception(\
+				toStr(__FILE__) + toStr(":") + toStr(__LINE__) + toStr(" ") + message); \
+	}
 
-#define DBG_FAILIF(cond, exception, message) \
-if(cond) \
-{ \
-	throw exception( \
-	toStr(__FILE__)+toStr(":")+toStr(__LINE__)+toStr(" ")+message); \
-}
+ #define DBG_FAILIF(cond, exception, message) \
+	if (cond) \
+	{ \
+		throw exception(\
+				toStr(__FILE__) + toStr(":") + toStr(__LINE__) + toStr(" ") + message); \
+	}
 
-#define DBG_WARNING(cond, message) \
-if(cond) \
-{ \
-	cout << "Warning (line " << __LINE__ << " in " << __FILE__ << "): " << message << endl; \
-}
+ #define DBG_WARNING(cond, message) \
+	if (cond) \
+	{ \
+		cout << "Warning (line " << __LINE__ << " in " << __FILE__ << "): " << message << endl; \
+	}
 
-#define DBG_DO(dbgCode, expr) \
-if(debug(dbgCode)){ expr; }
+ #define DBG_DO(dbgCode, expr) \
+	if (debug(dbgCode)) { expr; }
 
-#define DBG_DO_( expr) expr
+ #define DBG_DO_(expr) expr
 
-#else										  // optimized mode
+#else                                                                             // optimized mode
 
-#define DBG_ASSERT(cond, exception, message)
-#define DBG_FAILIF(cond, exception, message)
-#define DBG_WARNING(cond, message)
-#define DBG_DO(dbgCode, expr)
-#define DBG_DO_(expr)
+ #define DBG_ASSERT(cond, exception, message)
+ #define DBG_FAILIF(cond, exception, message)
+ #define DBG_WARNING(cond, message)
+ #define DBG_DO(dbgCode, expr)
+ #define DBG_DO_(expr)
 #endif
 
 #ifdef SIMUMPI
-#define PENDING_MPI DBG_FAILIF(true, SystemError, "This function is not available in MPI modules")
+ #define PENDING_MPI DBG_FAILIF(true, SystemError, "This function is not available in MPI modules")
 #else
-#define PENDING_MPI
+ #define PENDING_MPI
 #endif
-				// definition for all mode
+// definition for all mode
 
-				// epsilon when during floating point comparison
+// epsilon when during floating point comparison
 #define cmp_epsilon (1.e-9)
 
 #define CLEARFLAG(var) (var = 0)
@@ -336,19 +356,19 @@ if(debug(dbgCode)){ expr; }
 #define RESETFLAG(var, flag) (var &= ~flag)
 #define ISSETFLAG(var, flag) (!!(var & flag))
 
-				// check range.
-#define CHECKRANGEPLOIDY(p)  DBG_FAILIF( p>=ploidy(), IndexError, "index (" + toStr(p) + ") out of range of ploidy of 0 ~ " + toStr(ploidy()-1))
-#define CHECKRANGESEX(sex) DBG_FAILIF( sex!=Male && sex!=Female, IndexError, "Wrong sex info. Male " + toStr(Male) + " or Fenamle "  + toStr(Female) + " only.")
-#define CHECKRANGESUBPOP(subPop) DBG_FAILIF ( subPop >= numSubPop(), IndexError, "Subpop index (" + toStr(subPop) + ") out of range of 0  - " + toStr(numSubPop()-1))
-#define CHECKRANGECHROM(chrom)   DBG_FAILIF ( chrom >= numChrom(), IndexError, "chromosome index (" + toStr(chrom) + ") out of range of 0 - " + toStr(numChrom()-1))
-#define CHECKRANGELOCUS(chrom, locus) DBG_FAILIF( locus >= numLoci(chrom), IndexError, "locus index (" + toStr(chrom) + ") out of range of 0 - " + toStr(numLoci(chrom)-1))
-#define CHECKRANGEABSLOCUS(locus) DBG_FAILIF(  locus >= totNumLoci(), IndexError, "absolute locus index (" + toStr(locus) + ") out of range of 0 - " + toStr(totNumLoci()-1))
-#define CHECKRANGEGENOSIZE(p) DBG_FAILIF( p>=genoSize(),IndexError, "locus index  (" + toStr(p) + ") out of range of 0 - " + toStr(genoSize()-1))
-#define CHECKRANGESUBPOPMEMBER(ind,sp) DBG_FAILIF( subPopSize(sp)>0 && ind >= subPopSize(sp), IndexError, "individual index (" + toStr(ind) + ") out of range 0 ~" + toStr(subPopSize(sp)-1) + " in subpopulation " + toStr(sp))
-#define CHECKRANGEIND(ind) DBG_FAILIF(ind >= popSize(), IndexError, "individual index (" + toStr(ind) + ") is out of range of 0 ~ " + toStr(popSize()-1))
-#define CHECKRANGEINFO(ind) DBG_FAILIF(ind >= infoSize(), IndexError, "info index (" + toStr(ind) + ") is out of rage of 0 ~ " + toStr(infoSize()-1))
+// check range.
+#define CHECKRANGEPLOIDY(p)  DBG_FAILIF(p >= ploidy(), IndexError, "index (" + toStr(p) + ") out of range of ploidy of 0 ~ " + toStr(ploidy() - 1))
+#define CHECKRANGESEX(sex) DBG_FAILIF(sex != Male && sex != Female, IndexError, "Wrong sex info. Male " + toStr(Male) + " or Fenamle "  + toStr(Female) + " only.")
+#define CHECKRANGESUBPOP(subPop) DBG_FAILIF(subPop >= numSubPop(), IndexError, "Subpop index (" + toStr(subPop) + ") out of range of 0  - " + toStr(numSubPop() - 1))
+#define CHECKRANGECHROM(chrom)   DBG_FAILIF(chrom >= numChrom(), IndexError, "chromosome index (" + toStr(chrom) + ") out of range of 0 - " + toStr(numChrom() - 1))
+#define CHECKRANGELOCUS(chrom, locus) DBG_FAILIF(locus >= numLoci(chrom), IndexError, "locus index (" + toStr(chrom) + ") out of range of 0 - " + toStr(numLoci(chrom) - 1))
+#define CHECKRANGEABSLOCUS(locus) DBG_FAILIF(locus >= totNumLoci(), IndexError, "absolute locus index (" + toStr(locus) + ") out of range of 0 - " + toStr(totNumLoci() - 1))
+#define CHECKRANGEGENOSIZE(p) DBG_FAILIF(p >= genoSize(), IndexError, "locus index  (" + toStr(p) + ") out of range of 0 - " + toStr(genoSize() - 1))
+#define CHECKRANGESUBPOPMEMBER(ind, sp) DBG_FAILIF(subPopSize(sp) > 0 && ind >= subPopSize(sp), IndexError, "individual index (" + toStr(ind) + ") out of range 0 ~" + toStr(subPopSize(sp) - 1) + " in subpopulation " + toStr(sp))
+#define CHECKRANGEIND(ind) DBG_FAILIF(ind >= popSize(), IndexError, "individual index (" + toStr(ind) + ") is out of range of 0 ~ " + toStr(popSize() - 1))
+#define CHECKRANGEINFO(ind) DBG_FAILIF(ind >= infoSize(), IndexError, "info index (" + toStr(ind) + ") is out of rage of 0 ~ " + toStr(infoSize() - 1))
 #ifdef SIMUMPI
-#define CHECKRANGELOCALINFO(ind) DBG_FAILIF(ind >= localInfoSize(), IndexError, "Local info index (" + toStr(ind) + ") is out of rage of 0 ~ " + toStr(localInfoSize()-1))
+ #define CHECKRANGELOCALINFO(ind) DBG_FAILIF(ind >= localInfoSize(), IndexError, "Local info index (" + toStr(ind) + ") is out of rage of 0 ~ " + toStr(localInfoSize() - 1))
 #endif
 }
 #endif
