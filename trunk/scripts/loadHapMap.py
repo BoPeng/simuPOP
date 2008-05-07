b3b6bbec12ae6179908a8fb1b2aab95cdc903586
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2008-05-07 03:06:52 +0000

    Read and set genetic map for loaded hapmap data

--- trunk/scripts/loadHapMap.py
+++ trunk/scripts/loadHapMap.py
@@ -7,6 +7,11 @@ I chose consensus data because these SNPs exist in all three populations,
 and I chose phased data because this information is important for the 
 followup applications.
 
+Estimated fine-scale recombination rates between adjacent markers are
+also downloaded from http://ftp.hapmap.org/recombination/2006-10_rel21_phaseI+II/rates/
+The result is saved in a population dictionary as genDist, which stores
+the map distance of each locus.
+
 Run this script in the directory. You will obtain files named hapmap_XX.bin,
 where XX is chromosome number. These populations can be loaded and used in
 followup applications. You should of course tailor the population (choose SNP
@@ -44,30 +49,33 @@ from simuPOP import *
 import os, sys, urllib, gzip, exceptions
 
 # URL and revision number. You can choose to download other hapmap files
-URL = 'http://www.hapmap.org/downloads/phasing/2006-07_phaseII/consensus/'
+Genotype_URL = 'http://www.hapmap.org/downloads/phasing/2006-07_phaseII/consensus/'
+Recom_URL = 'http://ftp.hapmap.org/recombination/2006-10_rel21_phaseI+II/rates/'
 rev = 21
 legend_freq_file = 'genotypes_chr%d_r%d_nr_fwd_consensus_legend+freq.gz'
-genotype_file =  'genotypes_chr%d_%s_r%d_nr_fwd_phased_consensus.gz'
+genotype_file = 'genotypes_chr%d_%s_r%d_nr_fwd_phased_consensus.gz'
+recom_file = 'genetic_map_chr%s.txt.gz'
 
-def downloadIfNeeded(file):
+def downloadIfNeeded(URL, path, file):
     '''Download file from hapmap website'''
-    if os.path.isfile(file):
-        return
-    print 'Downloading %s ...' % file
-    urllib.urlretrieve('%s/%s' % (URL, file), file)
-    if not os.path.isfile(file):
+    diskfile = os.path.join(path, file)
+    if os.path.isfile(diskfile):
+        return        
+    print 'Downloading %s to %s...' % (file, path)
+    urllib.urlretrieve('%s/%s' % (URL, file), diskfile)
+    if not os.path.isfile(diskfile):
         raise exceptions.SystemError('Failed to download file %s from URL %s' \
             % (file, URL))
 
 
-def getLoci(ch):
+def getLoci(ch, dest):
     '''Loci information is retrieved from _consensus_legend+freq files'''
     lociPos = []
     lociName = []
     file = legend_freq_file % (ch, rev)
-    downloadIfNeeded(file)
+    downloadIfNeeded(Genotype_URL, dest, file)
     try:
-        for line in gzip.open(file).readlines():
+        for line in gzip.open(os.path.join(dest, file)).readlines():
             fields = line.split()
             lociName.append(fields[0])
             # translate pos from index to cM. This is tentative
@@ -80,15 +88,15 @@ def getLoci(ch):
     return (lociPos, lociName)
 
 
-def getPopSize(numLoci, ch):
+def getPopSize(numLoci, ch, dest):
     '''Get population size of each subpopulations
       This function also checks if each line has desired number of alleles.
     '''
     count = [0]*3
     ll = 0
     ceu = genotype_file % (ch, 'CEU', rev)
-    downloadIfNeeded(ceu)
-    for line in gzip.open(ceu).readlines():
+    downloadIfNeeded(Genotype_URL, dest, ceu)
+    for line in gzip.open(os.path.join(dest, ceu)).readlines():
         if (ll == 0 and len(line.split()) != numLoci) or \
             (ll != 0 and len(line) != ll):
             print "Number of loci does not match in %s " % ceu
@@ -98,8 +106,8 @@ def getPopSize(numLoci, ch):
         count[0] += 1
     ll = 0
     yri = genotype_file % (ch, 'YRI', rev)
-    downloadIfNeeded(yri)
-    for line in gzip.open(yri).readlines():
+    downloadIfNeeded(Genotype_URL, dest, yri)
+    for line in gzip.open(os.path.join(dest, yri)).readlines():
         if (ll == 0 and len(line.split()) != numLoci) or \
             (ll != 0 and len(line) != ll):
             print "Number of loci does not match in %s " % yri
@@ -109,8 +117,8 @@ def getPopSize(numLoci, ch):
         count[1] += 1
     ll = 0
     jpt_chb = genotype_file % (ch, 'JPT+CHB', rev)
-    downloadIfNeeded(jpt_chb)
-    for line in gzip.open(jpt_chb).readlines():
+    downloadIfNeeded(Genotype_URL, dest, jpt_chb)
+    for line in gzip.open(os.path.join(dest, jpt_chb)).readlines():
         if (ll == 0 and len(line.split()) != numLoci) or \
             (ll != 0 and len(line) != ll):
             print "Number of loci does not match in %s " % jpt_chb
@@ -121,7 +129,7 @@ def getPopSize(numLoci, ch):
     return [x/2 for x in count]       
     
 
-def load_population(pop, ch, type):
+def load_population(pop, ch, type, dest):
     '''Load population from file, with type (subpopulation type)'''
 # For the CEU and YRI the haplotypes are arranged as follows:
 #  
@@ -150,9 +158,9 @@ def load_population(pop, ch, type):
 # 
     subPop = {'CEU':0, 'YRI':1, 'JPT+CHB':2}[type]
     file = genotype_file % (ch, type, rev)
-    downloadIfNeeded(file)
+    downloadIfNeeded(Genotype_URL, dest, file)
     print 'from %s...' % file
-    for line_no,line in enumerate(gzip.open(file).readlines()):
+    for line_no,line in enumerate(gzip.open(os.path.join(dest, file)).readlines()):
         genotype = [int(x) for x in line.split()]
         ind = line_no / 2
         ploidy = line_no % 2
@@ -162,14 +170,76 @@ def load_population(pop, ch, type):
             ind.setAllele(g, i, ploidy)
 
 
+def set_map_dist(pop, ch, dest):
+    '''Set map distance for each locus'''
+    file = recom_file % ch
+    downloadIfNeeded(Recom_URL, dest, file)
+    print 'Reading genetic map file %s...' % file,
+    dist = {}
+    for line in gzip.open(os.path.join(dest, file)).readlines():
+        try:
+            fields = line.split()
+            pos = int(fields[0])
+            dist[pos] = float(fields[2])
+        except:
+            pass
+    # now, try to set genetic map
+    map = {}
+    for loc in range(pop.totNumLoci()):
+        pos = int(1000000*pop.locusPos(loc))
+        name = pop.locusName(loc)
+        try:
+            map[name] = dist[pos]
+        except:
+            pass
+    print ' map distance of %d markers are found' % len(map)
+    prev = -1     # name of the previous marker with map distance
+    next = -1     # name of the next marker with map distance
+    cnt = 0
+    for loc in range(pop.totNumLoci()):
+        pos = pop.locusPos(loc)
+        name = pop.locusName(loc)
+        if map.has_key(name):
+            prev = next = loc
+        else:
+            cnt += 1
+            # if it is before the first one, map distance is 0. 
+            if prev == -1:
+                map[name] = 0.
+                continue
+            # if it is after the last one, map distance is the last one
+            elif next == -1:
+                map[name] = map[pop.locusName(prev)]
+                continue
+            # neither prev of next is ''
+            if prev == next: # find the next!!
+                next = -1
+                for n in range(loc+1, pop.totNumLoci()):
+                    if map.has_key(pop.locusName(n)):
+                        next = n
+                        prev_pos = pop.locusPos(prev)
+                        next_pos = pop.locusPos(next)
+                        prev_dis = map[pop.locusName(prev)]
+                        next_dis = map[pop.locusName(next)]
+                        break
+            # if not found
+            if next == -1:
+                map[name] = map[pop.locusName(prev)]
+                continue
+            else:
+                map[name] = map[pop.locusName(prev)] + (pos - prev_pos) / (next_pos - prev_pos) * (next_dis - prev_dis)
+    print 'Map distance of %d markers (%.2f%% of %d) are estimated' % (cnt, cnt*100.0/pop.totNumLoci(), pop.totNumLoci())
+    pop.dvars().genDist =genp
+
+    
 def loadHapMap(chroms, dest='.'):
     '''Download, import and save hapmap data of given chromosomes'''
     ps = [0,0,0]
     for ch in chroms:
         popFile = "hapmap_%d.bin" % ch
         print "\n\nCreating population %s" % popFile
-        (lociPos, lociName) = getLoci(ch)
-        popSize = getPopSize(len(lociPos), ch)
+        (lociPos, lociName) = getLoci(ch, dest)
+        popSize = getPopSize(len(lociPos), ch, dest)
         if ps[0] == 0:
             ps = popSize
         else:
@@ -177,14 +247,16 @@ def loadHapMap(chroms, dest='.'):
                 print "Population size does not match across chromosomes"
                 sys.exit(1)
         pop = population(size=popSize, ploidy=2, loci=[len(lociPos)],
-            lociPos=lociPos, lociNames=lociName)
-        print "Loading CEU population",
-        load_population(pop, ch, type='CEU')
-        print "Loading YRI population",
-        load_population(pop, ch, type='YRI')
-        print "Loading JPT+CHB population",
-        load_population(pop, ch, type='JPT+CHB')
-        print "Calculating allele frequency ..."
+            lociPos=lociPos, lociNames=lociName, chromNames=[str(ch)])
+        print 'Loading CEU population',
+        load_population(pop, ch, type='CEU', dest=dest)
+        print 'Loading YRI population',
+        load_population(pop, ch, type='YRI', dest=dest)
+        print 'Loading JPT+CHB population',
+        load_population(pop, ch, type='JPT+CHB', dest=dest)
+        print 'Setting genetic distance...'
+        set_map_dist(pop, ch, dest)
+        print 'Calculating allele frequency ...'
         Stat(pop, alleleFreq=range(pop.totNumLoci()))
         print "Saving population to %s..." % popFile
         SavePopulation(pop, os.path.join(dest, popFile))
