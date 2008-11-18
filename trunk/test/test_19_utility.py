a461d57528a2b2ebcefc86fc067c443fdaba7cd0
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2008-11-18 03:11:39 +0000

    Test terminator

--- trunk/test/test_19_utility.py
+++ trunk/test/test_19_utility.py
@@ -18,6 +18,30 @@ import unittest, os, sys, exceptions
 
 class TestUtility(unittest.TestCase):
 
+    def interactiveTestPauseAtGen(self):
+        'Testing resume to simulation'
+        simu = simulator( population(size=10, ploidy=2, loci=[2, 3]),
+            randomMating(), rep=5)
+        print "\n\nUSER INTERACTION: Please press q\n\n"
+        self.assertRaises( exceptions.SystemError, simu.evolve,
+            ops=[ pause(at=[10]),
+                        # should quite, can not reach generation 12
+                        terminateIf("True", at=[12] ) ] )
+
+    def interactiveTestExitToShell(self):
+        'Testing exit to a shell'
+        simu = simulator( population(size=10, ploidy=2, loci=[2, 3]),
+            randomMating(), rep=5)
+        print "\n\nUSER INTERACTION: Please press s and then Ctrl-D"
+        print "Please check the existence of variable pop\n\n"
+        simu.evolve(
+            ops=[ pause(at=[10]) ], end=12)
+        print "\n\nUSER INTERACTION: Please press s and then Ctrl-D"
+        print "Please check the existence of variable tmpPop\n\n"
+        simu.evolve(
+            ops=[ pause(at=[20], popName='tmpPop') ], end=25)
+
+
     def testSetRNG(self):
         'Testing all RNG types'
         for rg in ListAllRNG():
