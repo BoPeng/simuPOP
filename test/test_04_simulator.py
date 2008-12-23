85f8c4f143db569a012cd27352ae6916d9ae14eb
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2008-12-23 02:52:20 +0000

    Fix simulator::populations and submit tests for simulators.

--- test/test_04_simulator.py
+++ test/test_04_simulator.py
@@ -13,58 +13,11 @@ simuOpt.setOptions(quiet=True)
 from simuPOP import *
 import unittest, os, sys
 
-# record active generations in pop.dvars().hist
-def genRecorder(pop):
-    try:
-        pop.dvars().hist.append(pop.dvars().gen)
-    except:
-        pop.dvars().hist = [pop.dvars().gen]
-    return True
-
-def opRecorder(*args, **kwargs):
-    return pyOperator(func=genRecorder, *args, **kwargs)
-
-
 class TestSimulator(unittest.TestCase):
 
-    def testPopulations(self):
-        'Testing set/get populations (FIXME)'
-        pop = population(size=2, loci=[1])
-        simu = simulator(pop, randomMating(), rep=2)
-        # pop is not affected if simu changes
-        simu.population(0).individual(0).setAllele(1,0)
-        self.assertEqual(pop.individual(0).allele(0), 0)
-        # if we get a reference,...
-        pop = simu.population(1)
-        pop.individual(0).setAllele(1,0)
-        self.assertEqual(simu.population(1).individual(0).allele(0), 1)
-        # if we get a real copy out, they are independent
-        pop1 = simu.extract(1)
-        simu.population(0).individual(1).setAllele(0,0)
-        self.assertEqual(pop1.individual(0).allele(0), 1)
-
-    def testProperties(self):
-        'Testing simulator properties'
-        pop = population(size=1, loci=[1])
-        simu = simulator(pop, randomMating(), rep=3)
-        self.assertEqual(simu.numRep(), 3)
-        self.assertEqual(simu.gen(), 0)
-        simu.setGen(10)
-        self.assertEqual(simu.gen(), 10)
-
-    def testEvolve(self):
-        'Testing function evolve and step'
-        pop = population(size=1, loci=[1])
-        simu = simulator(pop, randomMating(), rep=3)
-        self.assertEqual(simu.gen(), 0)
-        # no terminator, no ending generation is specified
-        self.assertRaises(exceptions.ValueError,
-            simu.evolve, ops=[] )
-
-
     def testClone(self):
-        'Testing cloning of simulator'
-        pop = population(size=[10, 4], loci=[2, 5])
+        'Testing simulator::clone() of simulator'
+        pop = population(size=[100, 40], loci=[2, 5])
         simu = simulator(pop, randomMating(), rep = 3)
         simu.evolve(
             preOps = [initByFreq([0.3, .7])],
@@ -77,73 +30,129 @@ class TestSimulator(unittest.TestCase):
         self.assertEqual(simu1.gen(), simu.gen())
         # test if a cloned simulator can evolve again
         simu1.evolve(
+            preOps = [initSex()],
             ops = [stat(alleleFreq=range(pop.totNumLoci()))],
             gen = 20
         )
 
+    def testSave(self):
+        'Testing simulator::save(filename)'
+        pop = population(size=[100, 40], loci=[2, 5])
+        simu = simulator(pop, randomMating(), rep = 3)
+        simu.evolve(
+            preOps = [initByFreq([0.3, .7])],
+            ops = [stat(alleleFreq=range(pop.totNumLoci()))],
+            gen = 10
+        )
 
+        simu.save("simuout");
+        simu1 = LoadSimulator("simuout", randomMating())
+        self.assertEqual(simu, simu1)
 
-    def testGenoStru(self):
-        'Testing genotypic structure related functions'
-        # genetic structure can also be accessed from simulator
-        if AlleleType() != 'binary':
-            pop = population(size=[20, 80], ploidy=2, loci=[5, 7],
-                lociPos=[ [2, 3, 4, 5, 6], [2, 4, 6, 8, 10, 12, 14]],
-                alleleNames=['_', 'A', 'C', 'T', 'G'])
-        else:
-            pop = population(size=[20, 80], ploidy=2, loci=[5, 7],
-                lociPos=[ [2, 3, 4, 5, 6], [2, 4, 6, 8, 10, 12, 14]],
-                alleleNames=['1', '2'])
-        #
-        simu = simulator(pop, cloneMating() )
-        #
-        self.assertEqual(simu.ploidy(), 2)
-        self.assertEqual(simu.ploidyName(), 'diploid')
-        self.assertEqual(simu.numChrom(), 2)
-        self.assertEqual(simu.numLoci(0), 5)
-        self.assertEqual(simu.numLoci(1), 7)
-        self.assertRaises(exceptions.IndexError, simu.numLoci, 2 )
-        self.assertEqual(simu.locusPos(10), 12)
-        self.assertRaises(exceptions.IndexError, simu.locusPos, 20 )
-        self.assertRaises((exceptions.TypeError, exceptions.OverflowError), simu.locusPos, -1 )
-        self.assertEqual(len(simu.lociPos()), 12)
-        self.assertEqual(list(simu.lociPos()), [2, 3, 4, 5, 6, 2, 4, 6, 8, 10, 12, 14])
-        self.assertEqual(simu.chromBegin(0), 0)
-        self.assertEqual(simu.chromBegin(1), 5)
-        self.assertEqual(simu.chromEnd(0), 5)
-        self.assertEqual(simu.chromEnd(1), 12)
-        self.assertRaises(exceptions.IndexError, simu.chromBegin, 2 )
-        self.assertRaises(exceptions.IndexError, simu.chromEnd, 2 )
-        self.assertEqual(simu.absLocusIndex(1, 5), 10)
-        self.assertEqual(simu.locusPos(simu.absLocusIndex(1, 2) ), 6)
-        self.assertRaises(exceptions.IndexError, simu.absLocusIndex, 2, 5 )
-        self.assertEqual(simu.chromLocusPair(10), (1, 5) )
-        self.assertRaises(exceptions.IndexError, simu.chromLocusPair, 50 )
-        self.assertEqual(simu.totNumLoci(), 12)
-        self.assertEqual(simu.genoSize(), simu.totNumLoci()*simu.ploidy() )
-        if AlleleType() == 'binary':
-            self.assertEqual(simu.alleleNames(), ('1', '2') )
-            self.assertEqual(simu.alleleName(0), '1')
-            self.assertEqual(simu.alleleName(1), '2')
-        else:
-            self.assertEqual(simu.alleleName(0), '_')
-            self.assertEqual(simu.alleleName(1), 'A')
-            self.assertEqual(simu.alleleName(2), 'C')
-            self.assertEqual(simu.alleleName(3), 'T')
-            self.assertEqual(simu.alleleName(4), 'G')
-        # loci name, default, the name will be used by other programs
-        # or file format, so we set it to be one based.
-        self.assertEqual(simu.locusName(0), 'loc1-1')
-        self.assertEqual(simu.locusName(1), 'loc1-2')
-        self.assertEqual(simu.locusName(5), 'loc2-1')
-        self.assertRaises(exceptions.IndexError, simu.locusName, 12)
+    def testSetGen(self):
+        'Testing simulator::gen(), setGen(gen)'
+        pop = population(size=1, loci=[1])
+        simu = simulator(pop, randomMating(), rep=3)
+        self.assertEqual(simu.numRep(), 3)
+        self.assertEqual(simu.gen(), 0)
+        simu.setGen(10)
+        self.assertEqual(simu.gen(), 10)
 
-    def testTerminator(self):
-        'Testing terminator'
-        simu = simulator(population(1), cloneMating() )
-        simu.evolve(ops=[terminateIf('gen==10' ) ] )
-        # always point to the enxt gen
-        self.assertEqual(simu.gen(), 11 )
+    def testEvolve(self):
+        'Testing simulator:: evolve()'
+        pop = population(size=1, loci=[1])
+        simu = simulator(pop, randomMating(), rep=3)
+        self.assertEqual(simu.gen(), 0)
+        # no terminator, no ending generation is specified
+        self.assertRaises(exceptions.ValueError,
+            simu.evolve, ops=[] )
+        # sample
+        simu = simulator(
+            population(size=[20, 80], loci=[3]), randomMating())
+        simu.evolve(
+            preOps = [initByFreq([0.2, 0.8])],
+            ops = [
+                recombinator(rate=0.001),
+                stat(alleleFreq=[1]),
+            ],
+            postOps = [stat()],
+            #dryrun=True,
+            gen=10
+        )
+
+    def testExtract(self):
+        'Testing simulator::extract(rep), numRep()'
+        pop = population(size=[20, 80], loci=[3])
+        simu = simulator(pop, randomMating(), rep=5)
+        repnum = simu.numRep()
+        simu.extract(2)
+        self.assertEqual(simu.numRep(), repnum-1)
+        self.assertRaises(exceptions.IndexError, simu.extract, 5)
+
+    def testPopulation(self):
+        'Testing simulator::population(rep), populations()'
+        pop = population(size=1000, loci=[1])
+        simu = simulator(pop, randomMating(), rep=5)
+        # pop is not affected if simu changes
+        for rep in range(5):
+            for idx in range(pop.popSize()):
+                simu.population(rep).individual(idx).setAllele(1, 0)
+                self.assertEqual(simu.population(rep).individual(idx).allele(0), 1)
+                self.assertEqual(pop.individual(idx).allele(0), 0)
+        # reference to the rep-th population
+        for rep in range(5):
+            pop = simu.population(rep)
+            for idx in range(pop.popSize()):
+                pop.individual(rep).setAllele(1, 0)
+                self.assertEqual(simu.population(rep).individual(idx).allele(0), 1)
+                self.assertEqual(pop.individual(idx).allele(0), 1)
+        # independent copy of the population
+        for rep in range(5):
+            pop1 = simu.population(rep).clone()
+            for idx in range(pop.popSize()):
+                simu.population(rep).individual(idx).setAllele(0, 0)
+                self.assertEqual(pop1.individual(idx).allele(0), 1)
+                self.assertEqual(simu.population(rep).individual(idx).allele(0), 0)
+        # populations
+        for onepop in simu.populations():
+            for idx in range(onepop.popSize()):
+                self.assertEqual(onepop.individual(idx).allele(0), 0)
+        self.assertRaises(exceptions.IndexError, simu.population, 5)
+
+    def testAddInfoField(self):
+        'Testing simulator::addInfoFields(field, init=0), addInfoFields(fields, init=0)'
+        'setAncestralDepth(depth), setMatingScheme(matingScheme)'
+        simu = simulator(population(100, infoFields=['a']), cloneMating(), rep=3)
+        simu.setMatingScheme(randomMating())
+        simu.addInfoField('b')
+        self.assertEqual(simu.infoFields(), ('a', 'b'))
+        simu.addInfoFields(['c', 'd'])
+        simu.setAncestralDepth(2)
+        simu.evolve(preOps = [initSex()], ops=[], gen=10)
+        self.assertEqual(simu.population(0).ancestralGens(), 2)
+        self.assertEqual(simu.infoFields(), ('a', 'b', 'c', 'd'))
+        self.assertEqual(simu.population(0).infoFields(), ('a', 'b', 'c', 'd'))
+        simu.population(0).addInfoField('l')
+        self.assertRaises(exceptions.ValueError, simu.addInfoField, 'j')
+        self.assertRaises(exceptions.ValueError, simu.evolve, ops=[])
+
+    def testVars(self):
+        'Testing simulator::vars(rep), vars(rep, subPop), dvars(rep), dvars(rep, subPop)'
+        pop = population(size=100, loci=[2, 4])
+        InitByFreq(pop, [.2, .3, .5])
+        Stat(pop, alleleFreq=range(0, 6))
+        simu = simulator(pop, randomMating(), rep=5)
+        for rep in range(5):
+            self.assertEqual(len(simu.population(rep).vars()["alleleFreq"]), 6)
+            self.assertEqual(len(simu.population(rep).dvars().alleleFreq), 6)
+        # with subPop
+        pop = population(size=[20, 80], loci=[2, 4])
+        InitByFreq(pop, [.2, .3, .5])
+        Stat(pop, alleleFreq=range(0, 6))
+        simu = simulator(pop, randomMating(), rep=5)
+        for rep in range(5):
+            self.assertEqual(len(simu.population(rep).vars(0)["alleleFreq"]), 6)
+            self.assertEqual(len(simu.population(rep).dvars(1).alleleFreq), 6)
 
     def testIntegrity(self):
         'Testing checking of simulator inegrity'
@@ -152,42 +161,6 @@ class TestSimulator(unittest.TestCase):
         # one can not change the genotype structure of the populations
         # in a simulator
         self.assertRaises(exceptions.ValueError, simu.evolve, ops=[])
-
-    def testMultiRep(self):
-        'Testing multi-replicates related functions'
-        simu = simulator(population(1), cloneMating(), rep=3 )
-        simu.evolve(
-            ops=[
-                opRecorder(),
-                terminateIf('gen==10', rep=0),
-                terminateIf('gen==15', rep=1),
-                terminateIf('gen==20', rep=2)
-            ]
-        )
-        # by default, run until the last resplicate die
-        self.assertEqual(simu.gen(), 21 )
-        self.assertEqual(simu.population(0).dvars().hist, range(11))
-        self.assertEqual(simu.population(1).dvars().hist, range(16))
-        self.assertEqual(simu.population(2).dvars().hist, range(21))
-        #
-
-
-    def testAddInfoField(self):
-        'Testing adding information fields to a simulator'
-        simu = simulator(population(10, infoFields=['a']), randomMating(), rep=3)
-        simu.addInfoField('b')
-        self.assertEqual(simu.infoFields(), ('a', 'b'))
-        simu.addInfoFields(['c', 'd'])
-        simu.setAncestralDepth(2)
-        simu.evolve(ops=[], gen=10)
-        self.assertEqual(simu.population(0).ancestralGens(), 2)
-        self.assertEqual(simu.infoFields(), ('a', 'b', 'c', 'd'))
-        self.assertEqual(simu.population(0).infoFields(), ('a', 'b', 'c', 'd'))
-        simu.population(0).addInfoField('l')
-        self.assertRaises(exceptions.ValueError, simu.addInfoField, 'j')
-        self.assertRaises(exceptions.ValueError, simu.evolve, ops=[])
-
-
 
 if __name__ == '__main__':
     unittest.main()
