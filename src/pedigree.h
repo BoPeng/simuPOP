5d51b4927d188d54bede355b646f975a3dbe1b54
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2008-12-24 17:42:12 +0000

    Move all constant definitions to simuPOP_cfg.h

--- src/pedigree.h
+++ src/pedigree.h
@@ -24,26 +24,11 @@
 #ifndef _PEDIGREE_H
 #define _PEDIGREE_H
 /**
- \file
- \brief head file of class pedigree
+   \file
+   \brief head file of class pedigree
  */
 #include "population.h"
 
-enum RelativeType {
-	REL_None,           // do nothing
-	REL_Self,           // individual himself or herself.
-	REL_Offspring,      // All offspring with all spouses (if there are more than one spouse)
-	REL_Spouse,         // All spouses (with at least one offspring)
-	REL_FullSibling,    // Siblings who share two parents
-	REL_Sibling,        // Siblings who share at least one parent
-};
-
-enum SexChoice {
-	AnySex = 0,
-	MaleOnly = 1,
-	FemaleOnly = 2,
-	OppositeSex = 3
-};
 
 namespace simuPOP {
 
@@ -56,7 +41,7 @@ public:
 	pedigree(const population & pop, const vectoru & loci = vectoru(),
 		const vectorstr & infoFields = vectorstr(), int ancGen = -1);
 
-	/** This function locates relatives (of type \e relType, and sex \e relSex) 
+	/** This function locates relatives (of type \e relType, and sex \e relSex)
 	 *  of each individual and store their indexes in specified information
 	 *  fields \e relFields. The indexes of parents in the parental generation
 	 *  should be available in information fields \e parentFields (default to
@@ -65,16 +50,16 @@ public:
 	 *  for diploid populations. \n
 	 *
 	 *  \param relType Relative type, which can be
-	 *  \li REL_Self set indexes of individual themselves.
-	 *  \li REL_Spouse locate spouses of individuals in the current generation.
+	 *  \li Self set indexes of individual themselves.
+	 *  \li Spouse locate spouses of individuals in the current generation.
 	 *    A spouse is defined as two individuals
-	   			having an offspring with shared \c parentFields. If more than one \c infoFields is given,
-	   			multiple spouses can be identified.
-	 \li REL_Offspring index of offspring in the offspring generation. If only one
-	   			parent is given, only paternal or maternal relationship is considered. For example,
-	   			<tt>parentFields=['father_idx']</tt> will locate offspring for all fathers.
-	 \li REL_FullSibling all siblings with the same parents
-	 \li REL_Sibling all sibs with at least one shared parent
+	            having an offspring with shared \c parentFields. If more than one \c infoFields is given,
+	            multiple spouses can be identified.
+	   \li Offspring index of offspring in the offspring generation. If only one
+	            parent is given, only paternal or maternal relationship is considered. For example,
+	            <tt>parentFields=['father_idx']</tt> will locate offspring for all fathers.
+	   \li FullSibling all siblings with the same parents
+	   \li Sibling all sibs with at least one shared parent
 	 * \param relFields information fields to hold relatives. The number of these fields
 	 *		limits the number of relatives to locate.
 	 * \param gen Find relatives for individuals for how many generations. Default to -1,
@@ -94,23 +79,23 @@ public:
 
 	/// Trace a relative path in a population and record the result in the given information fields.
 	/**
-	 \param pathGen A list of generations that form a relative path. This array is one element longer
-	   	than \c pathFields, with gen_i, gen_i+1 indicating the current and destinating generation
-	   	of information fields path_i.
-	 \param pathFields A list of list of information fields forming a path to trace a certain
-	   	type of relative.
-	 \param resultFields Where to store located relatives. Note that the result will be saved
-	   	in the starting generation specified in \c pathGen[0], which is usually 0.
-	 \param pathSex (Optional) A list of sex choices, AnySex, Male, Female or OppositeSex,
-	   	that is used to choose individuals at each step. Default to AnySex.
+	   \param pathGen A list of generations that form a relative path. This array is one element longer
+	    than \c pathFields, with gen_i, gen_i+1 indicating the current and destinating generation
+	    of information fields path_i.
+	   \param pathFields A list of list of information fields forming a path to trace a certain
+	    type of relative.
+	   \param resultFields Where to store located relatives. Note that the result will be saved
+	    in the starting generation specified in \c pathGen[0], which is usually 0.
+	   \param pathSex (Optional) A list of sex choices, AnySex, Male, Female or OppositeSex,
+	    that is used to choose individuals at each step. Default to AnySex.
 
 	   For example,
 	   <tt>
-	   	setInfoWithRelatives(pathGen = [0, 1, 1, 0],
-	   		pathFields = [['father_idx', 'mother_idx'], ['sib1', 'sib2'],
-	   			['off1', 'off2']],
-	   		pathSex = [AnySex, MaleOnly, FemaleOnly],
-	   		resultFields = ['cousin1', 'cousin2'])
+	    setInfoWithRelatives(pathGen = [0, 1, 1, 0],
+	        pathFields = [['father_idx', 'mother_idx'], ['sib1', 'sib2'],
+	            ['off1', 'off2']],
+	        pathSex = [AnySex, MaleOnly, FemaleOnly],
+	        resultFields = ['cousin1', 'cousin2'])
 	   </tt>
 	   This function will
 	   1. locate father_idx and mother_idx for each individual at generation 0 (\c pathGen[0])
@@ -120,7 +105,7 @@ public:
 	   5. find information fields \c off1 and \c off2 from these individuals, and
 	   6. locate FemaleOnly indiviudals referred by \c off1 and \of2 from geneartion 0 (\c pathGen[3])
 	   7. Save index of these individuals to information fields \c cousin1 and \c cousin2 at
-	   	genearation \c pathGen[0].
+	    genearation \c pathGen[0].
 
 	   In short, this function locates father or mother's brother's daughters.
 	 * <group>6-ancestral</group>
@@ -129,18 +114,19 @@ public:
 		const stringMatrix & pathFields,
 		const vectori & pathSex = vectori(),
 		const vectorstr & resultFields = vectorstr());
+
 };
 
 // /// A pedigree manipulation class
 // /**
 //    A pedigree has all the pedigree information that is needed to look at parent
 //    offspring relationship in a multi-generation population.
-// 
+//
 //    Conceptually, there are n generations with the latest generation being
 //    generation 0. The number of generations (c.f. gen()) is the number of
 //    parental generations plus 1. Therefore, each individual can be identified
 //    by (gen, idx).
-// 
+//
 //    Each individual can have a few properties
 //    1. mother (c.f. mother())
 //    2. father (c.f. father(), optional because a pedigree can have only one sex)
@@ -148,169 +134,169 @@ public:
 //    4. sex (c.f. info('sex'))
 //    5. affection (c.f. info('affection')
 //    6. arbitrary information fields (c.f. info())
-// 
+//
 //  */
 // class pedigree
 // {
 // private:
-// 	/// The reason why I do not want to use a
-// 	/// vector<vector< struct<dad, mom, vector<info>>>
-// 	/// structure is because mom and info are optional.
-// 	/// Because pedigree can be large, it makes sense to treat
-// 	/// them separately.
-// 
-// 	typedef vector<vectorlu> Pedigree;
-// 	// pedigree subpopulation size information ...
-// 	typedef Pedigree PedSize;
-// 	typedef vector<vector<vectorf> > PedInfo;
-// 
+//  /// The reason why I do not want to use a
+//  /// vector<vector< struct<dad, mom, vector<info>>>
+//  /// structure is because mom and info are optional.
+//  /// Because pedigree can be large, it makes sense to treat
+//  /// them separately.
+//
+//  typedef vector<vectorlu> Pedigree;
+//  // pedigree subpopulation size information ...
+//  typedef Pedigree PedSize;
+//  typedef vector<vector<vectorf> > PedInfo;
+//
 // public:
-// 	/// create a pedigree. If a filename \c pedfile is given, the pedgree
-// 	/// will be loaded from this file.
-// 	pedigree(int numParents = 2, const string & pedfile = string());
-// 
-// 	/// population size at generation \c gen
-// 	ULONG popSize(ULONG gen)
-// 	{
-// 		DBG_FAILIF(gen >= m_paternal.size(), IndexError,
-// 			"Generation number " + toStr(gen) + " out of bound (<"
-// 			+ toStr(m_paternal.size()) + ")");
-// 		return m_paternal[gen].size();
-// 	}
-// 
-// 
-// 	/// return the number of parents for each individual
-// 	UINT numParents()
-// 	{
-// 		return m_numParents;
-// 	}
-// 
-// 
-// 	/// Return the number of generations of this pedigree
-// 	ULONG gen()
-// 	{
-// 		return m_paternal.size();
-// 	}
-// 
-// 
-// 	/// Return the index of the father of individual \c idx at generation \c gen
-// 	/// The returned index is the absolute index of father in the parental generation
-// 	ULONG father(ULONG gen, ULONG idx);
-// 
-// 	/// Return the index of the mother of individual \c idx at generation \c gen
-// 	/// The returned index is the absolute index of mother in the parental generation
-// 	ULONG mother(ULONG gen, ULONG idx);
-// 
-// 	/// Return the index of the father of individual \c idx of subpopulation
-// 	/// \c subPop at generation \c gen
-// 	/// The returned index is the absolute index of father in the parental generation
-// 	ULONG father(ULONG gen, SubPopID subPop, ULONG idx);
-// 
-// 	/// Return the index of the mother of individual \c idx of subpopulation
-// 	/// \c subPop at generation \c gen
-// 	/// The returned index is the absolute index of mother in the parental generation
-// 	ULONG mother(ULONG gen, SubPopID subPop, ULONG idx);
-// 
-// 	/// Set the index of the father of individual  \c idx at generation \c gen
-// 	void setFather(ULONG parent, ULONG gen, ULONG idx);
-// 
-// 	/// Set the index of the mother of individual  \c idx at generation \c gen
-// 	void setMother(ULONG parent, ULONG gen, ULONG idx);
-// 
-// 	/// Set the index of the father of individual \c idx of subpopulation \c subPop at generation \c gen
-// 	void setFather(ULONG parent, ULONG gen, SubPopID subPop, ULONG idx);
-// 
-// 	/// Set the index of the mother of individual \c idx of subpopulation \c subPop at generation \c gen
-// 	void setMother(ULONG parent, ULONG gen, SubPopID subPop, ULONG idx);
-// 
-// 	/// Return information \c name of individual \c idx at generation \c gen
-// 	double info(ULONG gen, ULONG idx, const string & name);
-// 
-// 	/// Return information \c name of all individuals at generation \c gen
-// 	vectorf info(ULONG gen, const string & name);
-// 
-// 	/// Return information \c name of individual \c idx of subpopulation \c subPop at generation \c gen
-// 	double info(ULONG gen, SubPopID subPop, ULONG idx, const string & name);
-// 
-// 	/// Set information \c name  of individual \c idx at generation \c gen
-// 	void setInfo(double info, ULONG gen, ULONG idx, const string & name);
-// 
-// 	/// Set information \c name of individual \c idx of subpopulation \c subPop at generation \c gen
-// 	void setInfo(double info, ULONG gen, SubPopID subPop, ULONG idx, const string & name);
-// 
-// 	/// Add a generation to the existing pedigree, with given subpopulation sizes
-// 	/// \c subPopSize . All parental indexes and information will be set to zero
-// 	/// for the new generation.
-// 	void addGen(const vectorlu & subPopSize);
-// 
-// 	/// Return the subpopulation sizes of generation \c gen
-// 	vectorlu subPopSizes(ULONG gen);
-// 
-// 	/// Return the subpopulation size of subpopulation \c subPop of generation \c gen
-// 	ULONG subPopSize(ULONG gen, SubPopID subPop);
-// 
-// 	/// Make a copy of this pedigree
-// 	pedigree * clone()
-// 	{
-// 		return new pedigree();
-// 	}
-// 
-// 
-// 	/// load pedigree from a file, the file is usually saved by \c parentTagger or
-// 	/// \c parentsTagger. The format is described in the simuPOP reference manual
-// 	void load(const string & filename);
-// 
-// 	/// load information \c name from a information pedigree file and add to this pedigree
-// 	/// Information \c name should not have existed in the pedigree. The information
-// 	/// pedigree file \c filename is usually produced by taggers such as \c sexTagger
-// 	/// \c affectionTagger, \c pyTagger and \c infoTagger.
-// 	void loadInfo(const string & filename, const string & name);
-// 
-// 	/// load information \c names from a information pedigree file and add to this pedigree
-// 	/// Information names in \c names should not have existed in the pedigree.
-// 	/// pedigree file \c filename is usually produced by taggers such as \c sexTagger
-// 	/// \c affectionTagger, \c pyTagger and \c infoTagger.
-// 	void loadInfo(const string & filename, const vectorstr & names);
-// 
-// 	/// add an information field to the pedigree, with given initial value
-// 	void addInfo(const string & name, double init = 0);
-// 
-// 	/// Write the pedigree to a file
-// 	void save(const string & filename);
-// 
-// 	/// Save auxiliary information \c name to an information pedigree file
-// 	void saveInfo(const string & filename, const string & name);
-// 
-// 	/// save auxiliary information \c names to an information pedigree file
-// 	void saveInfo(const string & filename, const vectorstr & names);
-// 
-// 	/// Mark individuals \c inds as 'unrelated' in the last generation
-// 	/// \c removeUnrelated function will remove these individuals from the pdeigree
-// 	void selectIndividuals(const vectorlu & inds);
-// 
-// 	/// mark individuals that are unrelated to the visible individuals at the
-// 	/// last generation (not marked by selectIndividuals) from the pedigree.
-// 	void markUnrelated();
-// 
-// 	/// remove individuals that are unrelated to the last generation from the pedigree
-// 	/// indexes will be adjusted.
-// 	/// WARNING: if adjust_index=false, an invalid pedigree will be generated
-// 	void removeUnrelated(bool adjust_index = true);
-// 
+//  /// create a pedigree. If a filename \c pedfile is given, the pedgree
+//  /// will be loaded from this file.
+//  pedigree(int numParents = 2, const string & pedfile = string());
+//
+//  /// population size at generation \c gen
+//  ULONG popSize(ULONG gen)
+//  {
+//      DBG_FAILIF(gen >= m_paternal.size(), IndexError,
+//          "Generation number " + toStr(gen) + " out of bound (<"
+//          + toStr(m_paternal.size()) + ")");
+//      return m_paternal[gen].size();
+//  }
+//
+//
+//  /// return the number of parents for each individual
+//  UINT numParents()
+//  {
+//      return m_numParents;
+//  }
+//
+//
+//  /// Return the number of generations of this pedigree
+//  ULONG gen()
+//  {
+//      return m_paternal.size();
+//  }
+//
+//
+//  /// Return the index of the father of individual \c idx at generation \c gen
+//  /// The returned index is the absolute index of father in the parental generation
+//  ULONG father(ULONG gen, ULONG idx);
+//
+//  /// Return the index of the mother of individual \c idx at generation \c gen
+//  /// The returned index is the absolute index of mother in the parental generation
+//  ULONG mother(ULONG gen, ULONG idx);
+//
+//  /// Return the index of the father of individual \c idx of subpopulation
+//  /// \c subPop at generation \c gen
+//  /// The returned index is the absolute index of father in the parental generation
+//  ULONG father(ULONG gen, SubPopID subPop, ULONG idx);
+//
+//  /// Return the index of the mother of individual \c idx of subpopulation
+//  /// \c subPop at generation \c gen
+//  /// The returned index is the absolute index of mother in the parental generation
+//  ULONG mother(ULONG gen, SubPopID subPop, ULONG idx);
+//
+//  /// Set the index of the father of individual  \c idx at generation \c gen
+//  void setFather(ULONG parent, ULONG gen, ULONG idx);
+//
+//  /// Set the index of the mother of individual  \c idx at generation \c gen
+//  void setMother(ULONG parent, ULONG gen, ULONG idx);
+//
+//  /// Set the index of the father of individual \c idx of subpopulation \c subPop at generation \c gen
+//  void setFather(ULONG parent, ULONG gen, SubPopID subPop, ULONG idx);
+//
+//  /// Set the index of the mother of individual \c idx of subpopulation \c subPop at generation \c gen
+//  void setMother(ULONG parent, ULONG gen, SubPopID subPop, ULONG idx);
+//
+//  /// Return information \c name of individual \c idx at generation \c gen
+//  double info(ULONG gen, ULONG idx, const string & name);
+//
+//  /// Return information \c name of all individuals at generation \c gen
+//  vectorf info(ULONG gen, const string & name);
+//
+//  /// Return information \c name of individual \c idx of subpopulation \c subPop at generation \c gen
+//  double info(ULONG gen, SubPopID subPop, ULONG idx, const string & name);
+//
+//  /// Set information \c name  of individual \c idx at generation \c gen
+//  void setInfo(double info, ULONG gen, ULONG idx, const string & name);
+//
+//  /// Set information \c name of individual \c idx of subpopulation \c subPop at generation \c gen
+//  void setInfo(double info, ULONG gen, SubPopID subPop, ULONG idx, const string & name);
+//
+//  /// Add a generation to the existing pedigree, with given subpopulation sizes
+//  /// \c subPopSize . All parental indexes and information will be set to zero
+//  /// for the new generation.
+//  void addGen(const vectorlu & subPopSize);
+//
+//  /// Return the subpopulation sizes of generation \c gen
+//  vectorlu subPopSizes(ULONG gen);
+//
+//  /// Return the subpopulation size of subpopulation \c subPop of generation \c gen
+//  ULONG subPopSize(ULONG gen, SubPopID subPop);
+//
+//  /// Make a copy of this pedigree
+//  pedigree * clone()
+//  {
+//      return new pedigree();
+//  }
+//
+//
+//  /// load pedigree from a file, the file is usually saved by \c parentTagger or
+//  /// \c parentsTagger. The format is described in the simuPOP reference manual
+//  void load(const string & filename);
+//
+//  /// load information \c name from a information pedigree file and add to this pedigree
+//  /// Information \c name should not have existed in the pedigree. The information
+//  /// pedigree file \c filename is usually produced by taggers such as \c sexTagger
+//  /// \c affectionTagger, \c pyTagger and \c infoTagger.
+//  void loadInfo(const string & filename, const string & name);
+//
+//  /// load information \c names from a information pedigree file and add to this pedigree
+//  /// Information names in \c names should not have existed in the pedigree.
+//  /// pedigree file \c filename is usually produced by taggers such as \c sexTagger
+//  /// \c affectionTagger, \c pyTagger and \c infoTagger.
+//  void loadInfo(const string & filename, const vectorstr & names);
+//
+//  /// add an information field to the pedigree, with given initial value
+//  void addInfo(const string & name, double init = 0);
+//
+//  /// Write the pedigree to a file
+//  void save(const string & filename);
+//
+//  /// Save auxiliary information \c name to an information pedigree file
+//  void saveInfo(const string & filename, const string & name);
+//
+//  /// save auxiliary information \c names to an information pedigree file
+//  void saveInfo(const string & filename, const vectorstr & names);
+//
+//  /// Mark individuals \c inds as 'unrelated' in the last generation
+//  /// \c removeUnrelated function will remove these individuals from the pdeigree
+//  void selectIndividuals(const vectorlu & inds);
+//
+//  /// mark individuals that are unrelated to the visible individuals at the
+//  /// last generation (not marked by selectIndividuals) from the pedigree.
+//  void markUnrelated();
+//
+//  /// remove individuals that are unrelated to the last generation from the pedigree
+//  /// indexes will be adjusted.
+//  /// WARNING: if adjust_index=false, an invalid pedigree will be generated
+//  void removeUnrelated(bool adjust_index = true);
+//
 // private:
-// 	int m_numParents;
-// 
-// 	Pedigree m_paternal;
-// 	///
-// 	Pedigree m_maternal;
-// 	///
-// 	PedSize m_pedSize;
-// 	///
-// 	PedInfo m_info;
-// 	///
-// 	vectorstr m_infoNames;
+//  int m_numParents;
+//
+//  Pedigree m_paternal;
+//  ///
+//  Pedigree m_maternal;
+//  ///
+//  PedSize m_pedSize;
+//  ///
+//  PedInfo m_info;
+//  ///
+//  vectorstr m_infoNames;
 // };
-// 
+//
 
 
 }
