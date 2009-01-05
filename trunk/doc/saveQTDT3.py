ef19921cf597db5beeee55a8acc88ff849032234
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-01-05 16:44:46 +0000

    Remove individual python files and merge them into userGuide.py

--- trunk/doc/saveQTDT3.py
+++
@@ -1,6 +0,0 @@
-    # write map file
-    print >> mapOut, 'CHROMOSOME MARKER POSITION'
-    for marker in loci:
-        print >> mapOut, '%d\t%s\t%f' % (pop.chromLocusPair(marker)[0] + 1, 
-            pop.locusName(marker), pop.locusPos(marker))
-    mapOut.close()
