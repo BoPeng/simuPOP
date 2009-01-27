4dcf3a572b45c051a9b59614ef274d7331943af7
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-01-27 04:57:50 +0000

    Simplify convert.py because most of the implementation is sent to the converter trunk at Python.org

--- doc/tools/convert.py
+++ doc/tools/convert.py
@@ -47,102 +47,7 @@ class MyDocParser(DocParser):
     def __init__(self, *args, **kwargs):
         DocParser.__init__(self, *args, **kwargs)
 
-    #    self.handle_ifhtml = self.handle_ifhtml1
-    #
-    def handle_unrecognized(self, name):
-        def handler():
-            #print 'Unrecognized name: %s, use include directive to try to include an external file' % name
-            return InlineNode('include', name)
-        return handler
-
-    def parse_until(self, condition=None, endatbrace=False):
-        nodelist = NodeList()
-        bracelevel = 0
-        mathMode = False
-        math = ''
-        for l, t, v, r in self.tokens:
-            if condition and condition(t, v, bracelevel):
-                return nodelist.flatten()
-            if mathMode:
-                if t == 'mathmode':
-                    nodelist.append(InlineNode('math', [TextNode(math)]))
-                    math = ''
-                    mathMode = False
-                else:
-                    math += r
-            elif t == 'command':
-                if len(v) == 1 and not v.isalpha():
-                    nodelist.append(self.handle_special_command(v))
-                    continue
-                handler = getattr(self, 'handle_' + v, None)
-                if not handler:
-                    handler = self.handle_unrecognized(v)
-                nodelist.append(handler())
-            elif t == 'bgroup':
-                bracelevel += 1
-            elif t == 'egroup':
-                if bracelevel == 0 and endatbrace:
-                    return nodelist.flatten()
-                bracelevel -= 1
-            elif t == 'comment':
-                nodelist.append(CommentNode(v))
-            elif t == 'tilde':
-                nodelist.append(NbspNode())
-            elif t == 'mathmode':
-                mathMode = True
-            elif t == 'parasep':
-                nodelist.append(ParaSepNode())
-            else:
-                # includes 'boptional' and 'eoptional' which don't have a
-                # special meaning in text
-                nodelist.append(TextNode(v))
-        return nodelist.flatten()
- 
-    def parse_args(self, cmdname, argspec):
-        """ Helper to parse arguments of a command. """
-        # argspec: M = mandatory, T = mandatory, check text-only,
-        #          O = optional, Q = optional, check text-only
-        args = []
-        # ignore \[ \] for now
-        
-        def optional_end(type, value, bracelevel):
-            return type == 'eoptional' and bracelevel == 0
-
-        for i, c in enumerate(argspec):
-            assert c in 'OMTQ'
-            nextl, nextt, nextv, nextr = self.tokens.pop()
-            while nextt == 'comment' or (nextt == 'text' and nextv.isspace()):
-                nextl, nextt, nextv, nextr = self.tokens.pop()
-
-            if c in 'OQ':
-                if nextt == 'boptional':
-                    arg = self.parse_until(optional_end)
-                    if c == 'Q' and not isinstance(arg, TextNode):
-                        raise ParserError('%s: argument %d must be text only' %
-                                          (cmdname, i), nextl)
-                    args.append(arg)
-                else:
-                    # not given
-                    args.append(EmptyNode())
-                    self.tokens.push((nextl, nextt, nextv, nextr))
-                continue
-
-            if nextt == 'bgroup':
-                arg = self.parse_until(None, endatbrace=True)
-                if c == 'T' and not isinstance(arg, TextNode):
-                    raise ParserError('%s: argument %d must be text only' %
-                                      (cmdname, i), nextl)
-                args.append(arg)
-            else:
-                if nextt == 'command':
-                    nextl, nextt, nextv, nextr = self.tokens.pop()
-                elif nextt != 'text':
-                    raise ParserError('%s: non-grouped non-text arguments not '
-                                      'supported' % cmdname, nextl)
-                args.append(TextNode(nextv[0]))
-                self.tokens.push((nextl, nextt, nextv[1:], nextr[1:]))
-        return args
-
+  
     def mk_metadata_handler(self, name, mdname=None, arg='M'):
         if mdname is None:
             mdname = name
@@ -152,17 +57,6 @@ class MyDocParser(DocParser):
             return EmptyNode()
         return handler
 
-
-    def handle_special_command(self, cmdname):
-        if cmdname == '[':
-            math = ''
-            while True:
-                nextl, nextt, nextv, nextr = self.tokens.next()
-                if nextr == '\\]':
-                    break
-                math += nextr
-            return InlineNode('dispmath', math)
-        return DocParser.handle_special_command(self, cmdname)
 
     handle_color = mk_metadata_handler(None, 'color', None, 'M')
     handle_lstset = mk_metadata_handler(None, 'lstset')
@@ -195,25 +89,6 @@ class MyDocParser(DocParser):
     handle_bibliography = mk_metadata_handler(None, 'bibliography', None, 'O')
     handle_printindex = mk_metadata_handler(None, 'printindex', None, 'O')
 
-    handle_ne = mk_metadata_handler(None, 'ne', None, 'O')
-    handle_sum = mk_metadata_handler(None, 'sum', None, 'O')
-    handle_mu = mk_metadata_handler(None, 'mu', None, 'O')
-    handle_py = mk_metadata_handler(None, 'py', None, 'O')
-    handle_infty = mk_metadata_handler(None, 'infty', None, 'O')
-    handle_gg = mk_metadata_handler(None, 'gg', None, 'O')
-    handle_left = mk_metadata_handler(None, 'left', None, 'O')
-    handle_right = mk_metadata_handler(None, 'right', None, 'O')
-    handle_times = mk_metadata_handler(None, 'times', None, 'O')
-    handle_rightarrow = mk_metadata_handler(None, 'rightarrow', None, 'O')
-
-    def handle_textquoteright(self):
-        data = self.parse_args('\\textquoteright', 'M')
-        return TextNode("'")
-
-    def handle_textquoteleft(self):
-        data = self.parse_args('\\textquoteleft', 'M')
-        return TextNode("'")
-
     def handle_minipage_env(self):
         # Ignore the minipage part.
         txt = ''
@@ -244,150 +119,6 @@ class MyDocParser(DocParser):
 
     def handle_newcommand(self):
         return self.handle_newenvironment(2)
-    
-    #def handle_renewcommand(self):
-    #    return self.handle_newenvironment(2)
-
-    #def handle_title(self):
-    #    data = self.parse_args('\\title', 'M')
-    #    return SectioningNode('chapter', data)
-
-    def handle_lstinputlisting(self):
-        txt = ''
-        while not txt.endswith('\n'):
-            nextl, nextt, nextv, nextr = self.tokens.pop()
-            txt += nextr
-        try:
-            label = re.search('label={([^}]*)}', txt).groups()[0]
-            label = label.split(':')[-1]
-        except:
-            label = ''
-        try:
-            caption = re.search('caption={([^}]*)}', txt).groups()[0]
-            caption = caption.replace(label, '')
-        except:
-            caption = ''
-        try:
-            listing = re.search('\]{([^{]*)}', txt).groups()[0]
-        except:
-            listing = ''
-        try:
-            firstline = int(re.search('firstline=(\d+)', txt).groups()[0])
-        except:
-            firstline = None
-        try:
-            lastline = int(re.search('lastline=(\d+)', txt).groups()[0])
-        except:
-            lastline = None
-        # do not use this one yet
-        #return InlineNode('listing', '%s\n%s\n%s' % (listing, label, caption))
-        try:
-            src = open(listing)
-        except:
-            print("File ", listing, " can not be opened")
-            return EmptyNode()
-        txt = src.read()
-        try:
-            if lastline is not None:
-                txt = '\n'.join(txt.split('\n')[:(lastline+1)])
-            if firstline is not None:
-                txt = '\n'.join(txt.split('\n')[firstline-1:])
-        except:
-            pass
-        return NodeList([CommandNode('label', [TextNode(label)]),
-                InlineNode('strong', [TextNode('Example')]),
-                TextNode(': ' + caption),
-                VerbatimNode(TextNode(txt))])
-
-
-    def handle_figure_env(self):
-        txt = ''
-        while not txt.endswith('end{figure}'):
-            nextl, nextt, nextv, nextr = self.tokens.pop()
-            txt += nextr
-        try:
-            label = re.search('\\label{([^}]*)}', txt).groups()[0]
-            label = label.split(':')[-1]
-        except:
-            label = ''
-        try:
-            txt = txt.replace('\\label{fig:%s}' % label, '')
-            caption = re.search('\\caption{([^}]*)}', txt).groups()[0]
-        except:
-            caption = ''
-        try:
-            figure = re.search('\\includegraphics(\[.*\]){([^}]*)}', txt).groups()[1]
-        except:
-            figure = ''
-        try:
-            legend = ''
-            lines = txt.split('\n')
-            start = False
-            for l in lines:
-                if l.startswith('\\includegraphics'):
-                    start = True
-                    continue
-                if not start:
-                    continue
-                l = l.replace('\\end{centering}', '')
-                l = l.replace('\\end{figure}', '')
-                l = l.replace('\\par', '')
-                legend += l
-        except:
-            legend = ''
-        nodes = [CommandNode('label', [TextNode(label)])]
-        if caption != '':
-            nodes.extend([
-                InlineNode('strong', [TextNode('Figure')]),
-                TextNode(': '),
-                InlineNode('emph', [TextNode(caption.strip())])])
-        nodes.append(InlineNode('figure', figure))
-        if legend != '':
-            nodes.append(TextNode('\n\n' + legend))
-        return NodeList(nodes)
-
-    def handle_lstlisting_env(self):
-        txt = ''
-        opt = False
-        optText = ''
-        while not txt.endswith('end{lstlisting}'):
-            nextl, nextt, nextv, nextr = self.tokens.pop()
-            if nextr == '[':
-                opt = True
-            elif opt and nextr == ']':
-                opt = False
-                continue
-            if opt:
-                optText += nextr
-            else:
-                txt += nextr
-        # find label
-        try:
-            label = re.search('label=\{([^}]*)}', optText).groups()[0]
-            label = label.split(':')[-1]
-        except:
-            label = ''
-        try:
-            caption = re.search('caption=\{([^}]*)}', optText).groups()[0]
-            caption = caption.split(':')[-1]
-        except:
-            caption = ''
-        txtlist = txt.split('\n')
-        txt = ''
-        for t in txtlist:
-            if t.startswith('[cap') or '\\end{lstlisting}' in t:
-                continue
-            txt += t + '\n'
-        if label != '':
-            return NodeList([CommandNode('label', [TextNode(label)]),
-                InlineNode('strong', [TextNode('Example')]),
-                TextNode(': ' + caption),
-                VerbatimNode(TextNode(txt))])
-        return VerbatimNode(TextNode(txt))
-
-
-    def handle_centering_env(self):
-        return VerbatimNode(TextNode('a'))
 
     handle_small = mk_metadata_handler(None, '\\small', None, 'O')
     handle_ttfamily = mk_metadata_handler(None, '\\small', None, 'O')
@@ -413,20 +144,7 @@ class MyRestWriter(RestWriter):
 
     def visit_InlineNode(self, node):
         cmdname = node.cmdname
-        if cmdname == 'figure':
-            #self.flush_par()
-            figure = os.path.splitext(node.args)[0]
-            curPath = os.path.abspath('.')
-            os.chdir(self.dirname)
-            for path in ['.', '..', 'build']:
-                for suffix in ['', '.png', '.jpg', '.eps', '.pdf']:
-                    file = os.path.join(path, figure + suffix)
-                    if os.path.isfile(file):
-                        figure = file
-                        break
-            self.write('.. image:: %s\n   :width: 670\n' % figure )
-            os.chdir(curPath)
-        elif cmdname == 'listing':
+        if cmdname == 'listing':
             content = node.args[0]
             #self.flush_par()
             txt = node.args.split('\n')
@@ -449,64 +167,26 @@ class MyRestWriter(RestWriter):
         elif cmdname == 'ref':
             self.curpar.append('`%s%s`_' % (self.labelprefix,
                                                 text(node.args[0]).lower().split(':')[-1]))
-        elif cmdname == 'math':
-            math = node.args[0]
-            self.visit_wrapped(':math:`', math, '`')
-        elif cmdname == 'dispmath':
-            math = node.args
-            self.flush_par()
-            self.write('\n.. math::')
-            self.write('\n   '.join(math.split('\n')))
-            self.write('\n')
         else:
             RestWriter.visit_InlineNode(self, node)
+
+    def visit_CommandNode(self, node):
+        cmdname = node.cmdname
+        if cmdname == 'figure1':
+            self.write('.. image:: %s\n   :width: 670\n' % text(node.args[0]))
+            return
+        else:
+            RestWriter.visit_CommandNode(self, node)
 
     def visit_CommentNode(self, node):
         # no inline comments -> they are all output at the start of a new paragraph
         pass #self.comments.append(node.comment.strip())
 
-class MyTokenizer(Tokenizer):
-    def _tokenize(self):
-        lineno = 1
-        while not self.eos:
-            if self.scan(r'\\verb([^a-zA-Z])(.*?)(\1)'):
-                # specialcase \verb here
-                yield lineno, 'command', 'verb', '\\verb'
-                yield lineno, 'text', self.match.group(1), self.match.group(1)
-                yield lineno, 'text', self.match.group(2), self.match.group(2)
-                yield lineno, 'text', self.match.group(3), self.match.group(3)
-            elif self.scan(r'\\([a-zA-Z]+\*?)[ \t]*'):
-                yield lineno, 'command', self.match.group(1), self.mtext
-            elif self.scan(r'\\.'):
-                yield lineno, 'command', self.mtext[1], self.mtext
-            elif self.scan(r'\\\n'):
-                yield lineno, 'text', self.mtext, self.mtext
-                lineno += 1
-            elif self.scan(r'%(.*)\n[ \t]*'):
-                yield lineno, 'comment', self.match.group(1), self.mtext
-                lineno += 1
-            elif self.scan(r'[{}\[\]~$]'):
-                yield lineno, self.specials[self.mtext], self.mtext, self.mtext
-            elif self.scan(r'(\n[ \t]*){2,}'):
-                lines = self.mtext.count('\n')
-                yield lineno, 'parasep', '\n' * lines, self.mtext
-                lineno += lines
-            elif self.scan(r'\n[ \t]*'):
-                yield lineno, 'text', ' ', self.mtext
-                lineno += 1
-            ########## FIX A BUG HERE #####
-            elif self.scan(r'[^\\%}{\[\]~\n\$]+'):
-                yield lineno, 'text', self.mtext, self.mtext
-            else:
-                raise RuntimeError('unexpected text on line %d: %r' %
-                                   (lineno, self.data[self.pos:self.pos+100]))
-
-
     
 def convert_file(infile, outfile, doraise=True, splitchap=True,
                  toctree=None, deflang=None, labelprefix=''):
     inf = codecs.open(infile, 'r', 'latin1')
-    p = MyDocParser(MyTokenizer(inf.read()).tokenize(), infile)
+    p = MyDocParser(Tokenizer(inf.read()).tokenize(), infile)
     if not splitchap:
         outf = codecs.open(outfile, 'w', 'utf-8')
     else:
@@ -529,6 +209,7 @@ def convert_file(infile, outfile, doraise=True, splitchap=True,
             outf.close()
         else:
             outf.close()
+        p.finish()
         return 1, r.warnings
     except Exception, err:
         if doraise:
