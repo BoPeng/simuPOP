ae1348f6d9b6a1ab4fc69df14a5a2d131a2c0a8c
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-02-24 21:18:04 +0000

    Update license notification for source code

--- src/utility.h
+++ src/utility.h
@@ -1,25 +1,27 @@
-/***************************************************************************
-*   Copyright (C) 2004 by Bo Peng                                         *
-*   bpeng@rice.edu
-*                                                                         *
-*   $LastChangedDate$
-*   $Rev$                                                     *
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-*   This program is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-*   GNU General Public License for more details.                          *
-*                                                                         *
-*   You should have received a copy of the GNU General Public License     *
-*   along with this program; if not, write to the                         *
-*   Free Software Foundation, Inc.,                                       *
-*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
-***************************************************************************/
+/**
+ *  $File: utility.h $
+ *  $LastChangedDate$
+ *  $Rev$
+ *
+ *  This file is part of simuPOP, a forward-time population genetics
+ *  simulation environment. Please visit http://simupop.sourceforge.net
+ *  for details.
+ *
+ *  Copyright (C) 2004 - 2009 Bo Peng (bpeng@mdanderson.org)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef _UTILITY_H
 #define _UTILITY_H
