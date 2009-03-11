6bfa07620f42ad25ae0cadac83842e134a56b7bc
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-03-11 18:24:08 +0000

    Make simuHWE.py runnable

--- trunk/scripts/simuHWE.py
+++ trunk/scripts/simuHWE.py
@@ -15,12 +15,6 @@ allele frequencies in females and males are different.
 import simuOpt, os, sys, types, time
 
 options = [
-    {'arg':'h',
-     'longarg':'help',
-     'default':False,
-     'description':'Print this usage message.',
-     'jump':-1
-    },
     {'arg':'s:',
      'longarg':'size=',
      'default':100000,
@@ -54,55 +48,31 @@ options = [
      'description':'Initial allele frequency in females.',
      'validate':simuOpt.valueBetween(0, 1)
     },
-    {'longarg':'saveConfig=',
-     'default':'',
-     'allowedTypes':[types.StringType],
-     'description':'Save current paremeter set to specified file.'
-    },
-    {'arg':'v',
-     'longarg':'verbose',
-     'default':False,
-     'description':'Verbose mode.'},
-    ]
+]
 
 
 from simuPOP import *
 
 # get all parameters
-allParam = simuOpt.getParam(options, __doc__)
+par = simuOpt.simuOpt(options, __doc__)
+if not par.getParam():
+    sys.exit(1)
 
-if len(allParam) > 0:    # successfully get the params
-    (help, popSize, endGen, malleleFreq, falleleFreq, saveConfig, verbose) = allParam
-else:
-    sys.exit(0)
-
-if saveConfig != '':
-    simuOpt.saveConfig(options, saveConfig, allParam)
-
-if help:
-    print simuOpt.usage(options, __doc__)
-    sys.exit(0)
-
-# print out info if in verbose mode
-if verbose:
-    print "Pop size: ", popSize
-    print "End gen: ", endGen
-    print "Male allele frequency: ", malleleFreq
-    print "Female allele frequency: ", falleleFreq
+(popSize, endGen, malleleFreq, falleleFreq) = par.asList()
 
 # diploid population, one chromosome with 1 loci
 # random mating with sex
-simu = simulator(
-    population(size=popSize, ploidy=2, loci=[1]),
-    randomMating(),
-    )
+pop = population(size=popSize, ploidy=2, loci=[1])
+pop.setVirtualSplitter(sexSplitter())
+simu = simulator(pop, randomMating())
 
 # simulation
 print "p\tP00 (p^2)\tP01 (2p(1-p))\tP11 ((1-p)^2)"
 simu.evolve(
     preOps = [
-        initByFreq( maleFreq=1, indRange=[0,popSize/2], alleleFreq=[[malleleFreq, 1-malleleFreq]] ),
-        initByFreq( maleFreq=0, indRange=[popSize/2,popSize], alleleFreq=[[falleleFreq, 1-falleleFreq]] )
+        initSex(),
+        initByFreq(alleleFreq=[malleleFreq, 1-malleleFreq], subPops=[(0, 0)], initSex=False),
+        initByFreq(alleleFreq=[falleleFreq, 1-falleleFreq], subPops=[(0, 1)], initSex=False)
     ],
     ops = [
         stat(alleleFreq=[0], genoFreq=[0] ),
@@ -111,7 +81,7 @@ simu.evolve(
             "genoFreq[0][0][1], 2*alleleFreq[0][0]*(1-alleleFreq[0][0]), "\
             "genoFreq[0][1][1], (1-alleleFreq[0][0])*(1-alleleFreq[0][0]) )"),
     ],
-    end=endGen
+    gen=endGen
 )
 
 
