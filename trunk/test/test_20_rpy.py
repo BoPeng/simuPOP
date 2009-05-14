5807113f6ea5e7a7cb2f7f3556d4914e2a7a2cb2
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-05-14 04:14:08 +0000

    Update tests for the changes in varPlotter.

--- trunk/test/test_20_rpy.py
+++ trunk/test/test_20_rpy.py
@@ -40,8 +40,9 @@ class TestRPy(unittest.TestCase):
             ops = [
              migr,
              stator,
-             varPlotter('subPopSize', numRep=3, byRep=True,
-                 varDim=3, win=10, title='subPop size')
+             varPlotter('subPopSize', byRep=True, lty=[1, 2, 3],
+                type='l', win=10, main='subPop size', xlab='gen', ylim=[0, 100], 
+                ylab=['subPopSize (rep %d)' % x for x in range(3)])
              ],
              gen = 30
         )
@@ -63,21 +64,20 @@ class TestRPy(unittest.TestCase):
             ops = [
              migr,
              stator,
-             varPlotter('[x**2 for x in subPopSize]',
-                 numRep=3, byVal=True,
-                 varDim=3, win=10, title='subPop size')
+             varPlotter('[x**2 for x in subPopSize]', ylab='sp', type='l',
+                 byVal=True, win=10, main='subPop size')
              ],
              gen = 30
         )
         sleep(1)
         r.dev_off()
 
-    def testVarPlotterSeparate(self):
-        'Testing separate parameter of varPlotter'
+    def testVarPlotter(self):
+        'Testing byVal paramter of varPlotter'
         if not hasRPy:
             return True
         simu = simulator(
-            population(size=[50,50,100], ploidy=2, loci=[3,4], infoFields = ['migrate_to']),
+            population(size=[50, 50, 100], ploidy=2, loci=[3,4], infoFields = ['migrate_to']),
             randomMating(), rep=3)
         migr = migrator(rate=[[0,.2,.1],[.25,0,.1],[.1,.2,0]],
             mode=ByProbability)
@@ -87,11 +87,32 @@ class TestRPy(unittest.TestCase):
             ops = [
              migr,
              stator,
-             varPlotter('subPopSize', numRep=3,
-                 byVal=True,
-                 varDim=3, win=10,
-                 title='subPop size',
-                 separate=1)
+             varPlotter('[x**2 for x in subPopSize]', ylab='sp', type='l',
+                 win=10, main='subPop size', lty=range(1, 4))
+             ],
+             gen = 30
+        )
+        sleep(1)
+        r.dev_off()
+
+    def testVarPlotterByRepVal(self):
+        'Testing byVal paramter of varPlotter'
+        if not hasRPy:
+            return True
+        simu = simulator(
+            population(size=[50, 50, 100], ploidy=2, loci=[3,4], infoFields = ['migrate_to']),
+            randomMating(), rep=3)
+        migr = migrator(rate=[[0,.2,.1],[.25,0,.1],[.1,.2,0]],
+            mode=ByProbability)
+        stator = stat(popSize=1, stage=PreMating)
+        simu.evolve(
+            preOps = [initSex()],
+            ops = [
+             migr,
+             stator,
+             varPlotter('[x**2 for x in subPopSize]', ylab='sp', type='l', byVal=True, byRep=True,
+                 win=10, main=['subPop size %d' % x for x in range(9)],
+                 valArgs=['main'], lty=range(1, 4))
              ],
              gen = 30
         )
@@ -113,13 +134,14 @@ class TestRPy(unittest.TestCase):
             ops = [
              migr,
              stator,
-             varPlotter('[x**2 for x in subPopSize]', numRep=3, byRep=True,
-                 varDim=3, win=10, update=5, title='subPop size',
+             varPlotter('[x**2 for x in subPopSize]', byRep=True,
+                 win=10, update=5, main='subPop size', ylab='sp',
                  saveAs='demo')
              ],
              gen = 31
         )
         for f in range(5,31,5):
+            self.assertEqual(os.path.isfile('demo%d.eps' % f), True)
             os.remove('demo%d.eps' % f)
         sleep(1)
         r.dev_off()
@@ -139,112 +161,10 @@ class TestRPy(unittest.TestCase):
             ops = [
              migr,
              stator,
-             varPlotter('subPopSize', numRep=3, byRep=True, ylim=[0,100],
-                 separate=True, varDim=3, win=10, update=5, title='subPop size')
+             varPlotter('subPopSize', byRep=True, ylim=[0,100],
+                 ylab='sp', win=10, update=5, main='subPop size')
              ],
              gen = 30
-        )
-        sleep(1)
-        r.dev_off()
-
-    def testVarPlotterNoHistory(self):
-        'Testing no history parameter of varPlotter'
-        if not hasRPy:
-            return True
-        simu = simulator(
-            population(size=[50,50,100], ploidy=2, loci=[3,4], infoFields=['migrate_to']),
-            randomMating(), rep=3)
-        migr = migrator(rate=[[0,.2,.1],[.25,0,.1],[.1,.2,0]],
-            mode=ByProbability)
-        stator = stat(popSize=1, stage=PreMating)
-        simu.evolve(
-            preOps = [initSex()],
-            ops = [
-             migr,
-             stator,
-             varPlotter('[x**2 for x in subPopSize]',
-                 history=False,
-                 numRep=3, byRep=True,
-                 win=10, update=5, title='subPop size')
-             ],
-             gen = 30
-        )
-        sleep(1)
-        r.dev_off()
-
-    def testVarPlotterNoHisSeparate(self):
-        'Testing separate parameter in the no history case'
-        if not hasRPy:
-            return True
-        simu = simulator(
-            population(size=[50,50,100], ploidy=2, loci=[3,4], infoFields=['migrate_to']),
-            randomMating(), rep=3)
-        migr = migrator(rate=[[0,.2,.1],[.25,0,.1],[.1,.2,0]],
-            mode=ByProbability)
-        stator = stat(popSize=1, stage=PreMating)
-        simu.evolve(
-            preOps = [initSex()],
-            ops = [
-             migr,
-             stator,
-             varPlotter('[x**2 for x in subPopSize]', history=False,
-                 numRep=3, win=10, update=5, separate=1,
-                 title='subPop size')
-             ],
-             gen = 30
-        )
-        sleep(1)
-        r.dev_off()
-
-    def testVarPlotterImage(self):
-        'Testing the image parameter of varPlotter'
-        if not hasRPy:
-            return True
-        # test the operator
-        nr = 1
-        simu = simulator(
-            population(size=[50,50,100], ploidy=2, loci=[3,4], infoFields = ['migrate_to']),
-            randomMating(), rep=nr)
-        migr = migrator([[0,.2,.1],[.25,0,.1],[.1,.2,0]],
-                                        mode=ByProbability)
-        stator = stat(popSize=1, stage=PreMating)
-        simu.setGen(0)
-        simu.evolve(
-            preOps = [initSex()],
-            ops = [
-             migr,
-             stator,
-             varPlotter('subPopSize', history=True, numRep=nr, byRep=True,
-                 win=10, update=5, plotType='image', varDim=3, title='subPop size')
-             ],
-             gen = 10
-        )
-        sleep(1)
-        r.dev_off()
-
-    def testVarPlotterImageNoHistory(self):
-        'Testing no history version of image parameter'
-        if not hasRPy:
-            return True
-        # testing histroy = false and plotType='image'
-        numRep=4
-        popSize=500
-        #turnOnDebug(DBG_MUTATOR)
-        #turnOnDebug(DBG_SIMULATOR)
-        simu = simulator(population(size=popSize, loci=[10], infoFields = ['migrate_to']),
-            randomMating(), rep=numRep)
-        simu.evolve(
-            preOps = [ initByFreq([.2,.3,.5])],
-            ops = [
-                recombinator(rate=0.01),
-                # count number of affected
-                stat(LD=[ [x, y] for x in range(0,10) for y in range(0,10)]),
-                # plot affected status
-                varPlotter(expr='LD', plotType='image', numRep=4, byRep=True, update=5,
-                    title='pairwise LD', history=False),
-                # pause(rep=REP_LAST, step=5),
-            ],
-            gen = 20,
         )
         sleep(1)
         r.dev_off()
