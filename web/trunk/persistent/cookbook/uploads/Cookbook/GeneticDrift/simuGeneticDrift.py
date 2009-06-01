43ffb2bee38e3be9a65064c1cb988a4f9a374da5
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-06-01 21:58:15 +0000

    Various website changes

---
+++ web/trunk/persistent/cookbook/uploads/Cookbook/GeneticDrift/simuGeneticDrift.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+#
+# Demonstrate changes of allele frequency due to genetic drift. 
+
+"""
+This program demonstrates changes of allele frequency on single locus due to genetic drift.
+"""
+
+import simuOpt, os, sys, types, time
+from simuPOP import *
+
+try:
+    from simuRPy import *
+except:
+    print "simuRPy import failed. Please check your rpy installation."
+    print "Allele Frequencies will not be plotted"
+    useRPy = False
+else:
+    useRPy = True
+
+options = [
+    {'arg':'p:',
+     'longarg':'popSize=',
+     'default':100,
+     'label':'Population Size',
+     'allowedTypes':[types.IntType, types.LongType],
+     'validate':simuOpt.valueGT(0),
+     },
+    {'arg': 'P:',
+     'longarg':'p=',
+     'default':0.2,
+     'allowedTypes': [types.FloatType],
+     'label':'Initial Allele Frequency',
+     'validate':simuOpt.valueBetween(0., 1.),
+     },
+    {'arg':'g:',
+     'longarg':'generations=',
+     'default':100,
+     'label':'Number of Generations',
+     'allowedTypes':[types.IntType, types.LongType],
+     'validate':simuOpt.valueGT(0)
+     },
+    {'arg':'r:',
+     'longarg':'replications=',
+     'default':5,
+     'label':'Number of Replicates',
+     'allowedTypes':[types.IntType, types.LongType],
+     'validate':simuOpt.valueGT(0)
+     },
+]
+
+
+def simuGeneticDrift(popSize=100, p=0.2, generations=100, replications=5):
+    '''Simulate the Genetic Drift as a result of random mating.'''
+    # diploid population, one chromosome with 1 locus
+    # random mating with sex
+    pop = population(size=popSize, loci=[1])
+    simu=simulator(pop, randomMating(), rep=replications)
+
+    if useRPy:
+        plotter = varPlotter('alleleFreq[0][0]', ylim=[0, 1], ylab='allele frequency',
+            update=generations-1, saveAs='geneticDrift.png')
+    else:
+        plotter = noneOp()
+
+    # if number of generation is smaller than 200, step is 10 generations,
+    # if it's between 200 and 500, set step to be 20 generations,
+    # otherwise, step = 50 generations.
+    if generations <= 200:
+        s = 10
+    elif 200 < generations <= 500:
+        s = 20
+    else:
+        s = 50
+        
+    simu.evolve(
+        # everyone initially will have the same allele frequency
+        preOps=[initByFreq([p, 1-p])],
+        ops=[stat(alleleFreq=[0]),
+            pyEval(r'"Generation %d:\t" % gen', rep = 0, step = s),
+	    pyEval(r"'%.3f\t' % alleleFreq[0][0]", step = s),
+	    pyOutput('\n', rep=-1, step = s),
+	    plotter,
+            ],
+        gen = generations
+    )
+
+if __name__ == '__main__':
+    # get all parameters
+    pars = simuOpt.simuOpt(options, __doc__)
+    # cancelled
+    if not pars.getParam():
+        sys.exit(0)
+      
+    simuGeneticDrift(pars.popSize, pars.p, pars.generations, pars.replications)
+
+    # wait ten seconds before exit
+    if useRPy:
+        print "Figure will be closed after 10 seconds."
+        time.sleep(10)
+        
+    
+
+    
---
+++ web/trunk/persistent/cookbook/uploads/Cookbook/Migration/simuMigration.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+#
+# Demonstrate the changes of allele frequencies among subpopulations due to migration
+#
+
+"""
+This program demonstrates the effect of migration by rate which results in
+changes of allele frequencies among subpopulations.
+"""
+
+import simuOpt, os, sys, types, time
+from simuPOP import *
+
+try:
+    from simuRPy import *
+except:
+    print "simuRPy import failed. Please check your rpy installation."
+    print "allele frquencies in subpopulations will not be plotted"
+    useRPy = False
+else:
+    useRPy = True
+
+options = [
+    {
+     'longarg':'subPopSize=',
+     'default':5000,
+     'label':'Subpopulation Size',
+     'allowedTypes':[types.IntType, types.LongType],
+     'validate':simuOpt.valueGT(0),
+     },
+    {
+     'longarg':'numOfSubPops=',
+     'default':5,
+     'allowedTypes':[types.IntType, types.LongType],
+     'label':'Number of Subpopulations',
+     'validate':simuOpt.valueGT(0)
+     },
+    {
+     'longarg':'m=',
+     'default':0.05,
+     'label':'Migration Rate',
+     'allowedTypes':[types.FloatType],
+     'validate':simuOpt.valueBetween(0., 1.),
+     },
+    {
+     'longarg':'generations=',
+     'default':200,
+     'label':'Generations to evolve',
+     'description':'Length of evolution',
+     'allowedTypes':[types.IntType, types.LongType],
+     'validate':simuOpt.valueGT(0)
+     },
+]
+
+     
+def simuMigration(subPopSize, numOfSubPops, m, generations):
+    '''Simulate the change of allele frequencies among subpopulations as a result of migration.'''
+    # diploid population, one chromosome with 1 locus
+    # random mating with sex
+    pop = population(size=[subPopSize]*numOfSubPops, loci=[1], infoFields=['migrate_to'])
+    # set initial allele frequencies to each subpopulation
+    # Rule of initialization is to use a list of numbers, which have counts equal to number of
+    # subpopulations and been set evenly distributed between 0 and 1.
+    # Therefore, the average allele frequency named theoretical value among all subpopulations will be 0.5
+    for i in range(numOfSubPops):
+        InitByFreq(pop, [i*1./(numOfSubPops-1), 1 - i*1./(numOfSubPops-1)], subPops=[i])
+    simu = simulator(pop, randomMating())
+
+    # check if plot
+    if useRPy:
+        plotter = varPlotter('[0.5] + [subPop[i]["alleleFreq"][0][0] for i in range(%d)]' % numOfSubPops,
+            ylim=[0, 1], update=generations-1, legend=['Theoretical'] + ['Allele freq at subpop %d' % x for x in range(numOfSubPops)],
+	    xlab="generation", ylab="alleleFrequency", saveAs='migration.png')
+    else:
+        plotter = noneOp()
+        
+    a=[]
+    # set migration rate matrix a[]. Within each row i of matrix a[], all elements have the same value which is
+    # m divided by number of subpopulations minus one, except the diagonal element, whose value is set to be 0.
+    # This setting ensures that every individual in subpopulation i has probability m to become the migrator,
+    # and such a migrator has equal possibility of entering any other subpopulation. 
+    for i in range(numOfSubPops):
+        b=[];
+        for j in range(numOfSubPops):
+            if j==i:
+                b.append(0)
+            else:
+                b.append(m/(numOfSubPops-1))
+        a.append(b)
+    # if number of generations is smaller than 200, step will be set to 10 generations, otherwise it will be 20.
+    if generations <= 200:
+        s = 10
+    else:
+        s = 20
+    simu.evolve(
+        gen = generations,
+        ops = [
+            stat(alleleFreq=[0], stage=PreMating), 
+            migrator(rate = a),
+            pyEval(r'"Frequency at generation %d: %s\n" % (gen, ", ".join(["%.2f" % x for x in freq]))',
+                stmts = 'freq = [subPop[x]["alleleFreq"][0][0] for x in range(%i)]' % numOfSubPops, step = s, stage=PreMating),
+            plotter,
+            ],
+        )
+
+if __name__ == '__main__':
+    # get all parameters
+    pars = simuOpt.simuOpt(options, __doc__)
+    if not pars.getParam():
+        sys.exit(0)
+
+    simuMigration(pars.subPopSize, pars.numOfSubPops, pars.m, pars.generations)
+    #wait five seconds before exit
+    if useRPy:
+        print "Figure will be closed after five seconds."
+        time.sleep(5)
