d358591c6b029f5d7a3c39aaf34b61692f1298dd
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-06-05 01:52:20 +0000

    Let run_tests.py test all three kinds of modules.

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,38 +1,81 @@
 #!/usr/bin/env python
+
 #
-# This script finds all test scripts in the current directory and runs
-# them.
+# $File: run_tests.py $
+# $LastChangedDate$
+# $Rev$
 #
-# NOTE: to run test on different versions of libraries,
-#   setenv SIMUALLELETYPE short
-#                         long
-#                         binary
+# This file is part of simuPOP, a forward-time population genetics
+# simulation environment. Please visit http://simupop.sourceforge.net
+# for details.
 #
-# NOTE: you can stop testing test_20_rpy with an arbitrary commandline
-#   argument.
+# Copyright (C) 2004 - 2009 Bo Peng (bpeng@mdanderson.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+# This script finds all test scripts in the current directory and runs them.
+# 
+# Usage:
+#   run_tests.py
+#       Run tests for 'short', 'long', and 'binary' modules and summarize
+#       outputs
+#
+#   run_tests.py [ short | long | binary ]
+#       Run the tests for specified modules.
 #
 import dircache, re, unittest, sys, os
 
-# Find all files with names like test_MODULE.py.  For each such file,
-# import it as a module.  If it defines a function called "tests", call
-# that function and add its result to the test suite.
+def importTests():
+    tests = unittest.TestSuite()
 
-tests = unittest.TestSuite()
+    # Find all files with names like test_MODULE.py.  For each such file,
+    # import it as a module.  If it defines a function called "tests", call
+    # that function and add its result to the test suite.
+    for file in dircache.listdir('.'):
+        match = re.match("^(test_(.*))\\.py$", file)
+        if match:
+            m = match.group(1)
+            if len(sys.argv) > 2 and m == 'test_20_rpy':
+                continue
+            print "Adding test cases in ", m
+            module = __import__(m)
+            tests.addTest( unittest.defaultTestLoader.loadTestsFromModule( module ) )
+    return tests
 
-for file in dircache.listdir('.'):
-  match = re.match("^(test_(.*))\\.py$", file)
-  if match:
-    m = match.group(1)
-    if len(sys.argv) > 1 and m == 'test_20_rpy':
-      continue
-    print "Adding test cases in ", m
-    module = __import__(m)
-    tests.addTest( unittest.defaultTestLoader.loadTestsFromModule( module ) )
 
-# 3. RUN TESTS
-#
-# Set verbosity=2 to get more output.  Sadly it's not possible to get
-# the output of failed tests until all the tests are completed.
-
-test_runner = unittest.TextTestRunner(verbosity=2)
-test_runner.run(tests)
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        errors = {}
+        from subprocess import Popen, PIPE
+        for allele in ['binary', 'short', 'long']:
+            errors[allele] = []
+            p = Popen([sys.argv[0], allele], env={'SIMUALLELETYPE': allele},
+                shell=True, stderr=PIPE)
+            summary = False
+            for line in p.stderr.readlines():
+                print line,
+                if line.startswith('===================='):
+                    summary = True
+                if summary or not line.strip().endswith('ok'):
+                    errors[allele].append(line)
+        # summary
+        for allele in ['binary', 'short', 'long']:
+            print '\n\n%s\n=\n= Errors in the %s module\n=\n%s\n\n' % ('='*60, allele, '='*60)
+            for line in errors[allele]:
+                print line,
+    else:
+        test_runner = unittest.TextTestRunner(verbosity=2)
+        test_runner.run(importTests())
