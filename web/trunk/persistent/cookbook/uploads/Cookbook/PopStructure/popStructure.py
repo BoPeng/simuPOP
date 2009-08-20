f1bbff6a73a0ebc59a689a96aedfed8d070530ea
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-08-20 19:16:36 +0000

    Add pop structure example

---
+++ web/trunk/persistent/cookbook/uploads/Cookbook/PopStructure/popStructure.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from simuPOP import *
+from simuUtil import MigrIslandRates
+
+def calcFst(pop):
+    'Calculate Fst and Gst for the whole population and a random sample'
+    Stat(pop, structure=range(5), vars=['F_st', 'G_st'])
+    sample = RandomSample(pop, size=[500, 500, 500])[0]
+    Stat(sample, structure=range(5), vars=['F_st', 'G_st'])
+    print 'Gen: %3d Gst: %.3e (all), %.3e (sample) Fst: %.3e (all) %.3e (sample)' \
+        % (pop.dvars().gen, pop.dvars().G_st, sample.dvars().G_st,
+           pop.dvars().F_st, sample.dvars().F_st)
+    return True
+
+simu = simulator(population([10000]*3, loci=[1]*5, infoFields='migrate_to'),
+    randomMating())
+simu.evolve(
+    preOps = [
+        initByFreq([0.5, 0.5], loci=[0, 2]),
+        initByFreq([0.2, 0.4, 0.4], loci=[1, 3, 4]),
+    ],
+    ops = [
+        # migrator(rate=MigrIslandRates(0.01, 3)),
+        pyOperator(func=calcFst, step=20),
+    ],
+    gen = 500
+)
