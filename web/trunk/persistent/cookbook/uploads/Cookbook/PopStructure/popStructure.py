6c1551dd1cd16ac1d7509cda3235745de895126a
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-08-25 14:15:59 +0000

    Update web

--- web/trunk/persistent/cookbook/uploads/Cookbook/PopStructure/popStructure.py
+++ web/trunk/persistent/cookbook/uploads/Cookbook/PopStructure/popStructure.py
@@ -5,14 +5,15 @@ from simuUtil import MigrIslandRates
 def calcFst(pop):
     'Calculate Fst and Gst for the whole population and a random sample'
     Stat(pop, structure=range(5), vars=['F_st', 'G_st'])
-    sample = RandomSample(pop, size=[500, 500, 500])[0]
+    sample = RandomSample(pop, size=[500]*pop.numSubPop())[0]
     Stat(sample, structure=range(5), vars=['F_st', 'G_st'])
-    print 'Gen: %3d Gst: %.3e (all), %.3e (sample) Fst: %.3e (all) %.3e (sample)' \
-        % (pop.dvars().gen, pop.dvars().G_st, sample.dvars().G_st,
+    print 'Gen: %3d Gst: %.6f (all), %.6f (sample) Fst: %.6f (all) %.6f (sample)' \
+        % (pop.dvars().gen,
+           pop.dvars().G_st, sample.dvars().G_st,
            pop.dvars().F_st, sample.dvars().F_st)
     return True
 
-simu = simulator(population([10000]*3, loci=[1]*5, infoFields='migrate_to'),
+simu = simulator(population([10000]*2, loci=[1]*5, infoFields='migrate_to'),
     randomMating())
 simu.evolve(
     preOps = [
