3f8e23f77154597f4902fd05efba57f030839d28
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2009-09-11 03:07:10 +0000

    Minor documentation update

--- tools/doxy2swig.py
+++ tools/doxy2swig.py
@@ -442,7 +442,7 @@ class Doxy2SWIG:
              txt = '='.join(txt.split('=')[:-1])
         self.content[-1]['cppArgs'] = txt
         # @ is used instead of , to avoid separation of replaced text, it will be replaced back to ,
-        txt = re.sub(r'stringList\((["\w_]+),\s*(["\w_]+)\)', r'[\1@ \2]', txt)
+        txt = re.sub(r'stringList\((["\w]+),\s*(["\w]+)\)', r'[\1@ \2]', txt)
         txt = re.sub(r'vectorstr\(1, ([^)]+)\)', r'[\1]', txt)
         txt = re.sub(r'(.*)vector(str|u|i)\(1,\s*([\w"\d]+)\)(.*)', r'\1\3\4', txt)
         txt = re.sub(r'\)\s*const\s*$', ')', txt)
