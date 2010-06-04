4a0057bc385d03875b60f68b45424e51ad6d4982
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2010-06-04 02:41:10 +0000

    Prepare for a 1.0.3 release

--- src/population.cpp
+++ src/population.cpp
@@ -533,6 +533,7 @@ PyObject * Population::genotype(vspID vsp)
 void Population::setGenotype(const uintList & genoList, vspID subPop)
 {
 	const vectoru & geno = genoList.elems();
+
 	sortIndividuals();
 	if (!subPop.valid()) {
 		GenoIterator ptr = m_genotype.begin();
