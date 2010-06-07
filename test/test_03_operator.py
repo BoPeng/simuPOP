2905e7998860cc1468c5b7eeba990db6f834562f
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2010-06-07 18:03:55 +0000

    Update manifest

--- test/test_03_operator.py
+++ test/test_03_operator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# testing operator behaviors for simupoop.
+# testing operator behaviors for simupoop and general operators such as PyOperator and IfElse
 #
 # Bo Peng (bpeng@rice.edu)
 #
@@ -26,6 +26,11 @@ def opRecorder(*args, **kwargs):
     return PyOperator(func=genRecorder, *args, **kwargs)
 
 class TestOperator(unittest.TestCase):
+
+    def setUp(self):
+        self.pop = Population(size=10000, ploidy=2,
+            loci=[2, 3])
+        initSex(self.pop)
 
     def testActiveGen(self):
         'Testing active generation specifications'
@@ -245,5 +250,177 @@ class TestOperator(unittest.TestCase):
         os.remove('a.pop')
 
 
+    # define a function
+    def myFunc(self, pop):
+        stat(pop, alleleFreq=[0])
+        # allelic frequency was assigned to be 0.2
+        self.assertTrue(abs(pop.dvars().alleleFreq[0][0] - 0.2) < 0.05, 
+            "Expression abs(pop.dvars().alleleFreq[0][0] - 0.2) (test value %f) be less than 0.05. This test may occasionally fail due to the randomness of outcome." % (abs(pop.dvars().alleleFreq[0][0] - 0.2)))
+        return True
+
+    def testSimpleFunc(self):
+        'Testing Python operator'
+        initGenotype(self.pop, freq= [.2, .3, .5])
+        self.pop.evolve( postOps = PyOperator(self.myFunc),
+            matingScheme=RandomMating(),
+            gen=20)
+
+    def testCopyClone(self):
+        'Testing copy of python operator'
+        op = PyOperator(self.myFunc)
+        op1 = op
+        op2 = op.clone()
+        initGenotype(self.pop, freq= [.2, .3, .5])
+        # all copied version are working fine.
+        op.apply(self.pop)
+        op1.apply(self.pop)
+        op2.apply(self.pop)
+
+    def myFuncWithParam(self, pop, param):
+        ' para is (allele, freq) pair '
+        stat(pop, alleleFreq=[0])
+        self.assertTrue(abs(pop.dvars().alleleFreq[0][param[0]] - param[1]) < 0.05, 
+            "Expression abs(pop.dvars().alleleFreq[0][param[0]] - param[1]) (test value %f) be less than 0.05. This test may occasionally fail due to the randomness of outcome." % (abs(pop.dvars().alleleFreq[0][param[0]] - param[1])))
+        return True
+
+    def testFuncWithParam(self):
+        'Testing python operator with parameters'
+        initGenotype(self.pop, freq= [.2, .8])
+        self.pop.evolve( postOps=[
+            PyOperator(func=self.myFuncWithParam, param=(0,.2)),
+            PyOperator(func=self.myFuncWithParam, param=(1,.8)),
+            ],
+            matingScheme=RandomMating(),
+            gen=2
+        )
+
+    def myFuncAsTerminator(self, pop):
+        if pop.dvars().gen == 3:
+            return False
+        else:
+            return True
+
+    def testTerminator(self):
+        'Testing hybrid terminator'
+        simu = Simulator(self.pop)
+        simu.evolve(initOps = [InitSex()],
+            postOps = PyOperator(self.myFuncAsTerminator),
+            matingScheme=RandomMating(),
+            gen = 10 )
+        self.assertEqual(simu.dvars(0).gen, 4)
+
+    def dynaMutator(self, pop, param):
+        ''' this mutator mutate common loci with low mutation rate
+        and rare loci with high mutation rate, as an attempt to
+        bring allele frequency of these loci at an equal level.'''
+        # unpack parameter
+        (cutoff, mu1, mu2) = param;
+        stat(pop, alleleFreq=range( pop.totNumLoci() ) )
+        if moduleInfo()['alleleType'] == 'binary':
+            for i in range( pop.totNumLoci() ):
+                # 1-freq of wild type = total disease allele frequency
+                if 1-pop.dvars().alleleFreq[i][0] < cutoff:
+                    kAlleleMutate(pop, k=2, rates=mu1, loci=[i])
+                else:
+                    kAlleleMutate(pop, k=2, rates=mu2, loci=[i])
+        else:
+            for i in range( pop.totNumLoci() ):
+                # 1-freq of wild type = total disease allele frequency
+                if 1-pop.dvars().alleleFreq[i][1] < cutoff:
+                    kAlleleMutate(pop, k=2, rates=mu1, loci=[i])
+                else:
+                    kAlleleMutate(pop, k=2, rates=mu2, loci=[i])
+        return True
+
+    def testDynaMutator(self):
+        'Testing dynamic mutator (an example)'
+        simu = Simulator(self.pop)
+        simu.evolve(
+            initOps = [
+                InitGenotype(freq= [.6, .4], loci=[0,2,4]),
+                InitGenotype(freq= [.8, .2], loci=[1,3]) ],
+            matingScheme=RandomMating(),
+            postOps = [
+                PyOperator( func=self.dynaMutator, param=(.5, .1, 0) ),
+                Stat(alleleFreq=range(5)),
+                TerminateIf( 'alleleFreq[0][1] < 0.2' )
+                ],
+            gen = 30
+        )
+        # will not terminate when allele frequency get out of control
+        self.assertEqual(simu.dvars(0).gen, 30)
+
+
+    def testIfElseOperator(self):
+        'Testing opeartor IfElse'
+        pop = Population(1000, loci=[2])
+        # now if we want to flip a lot of alleles whenever it reaches 0.2
+        pop.evolve(
+            # init to 1,2 (or 0, 1 in the binary case)
+            initOps = [ InitSex(), InitGenotype(freq=[.5,.5]) ],
+            matingScheme = RandomMating(),
+            postOps = [
+                # count number of allels at this locus
+                Stat(alleleFreq=[0]),
+                # inject 50% of allele 2 if this allele get low freq
+                IfElse('alleleFreq[0][0]<0.2',
+                    KAlleleMutator(rates=.6, k=2, loci=[0]) ),
+                # the other way around?
+                IfElse('alleleFreq[0][0]>0.8',
+                    KAlleleMutator(rates=.6, k=2, loci=[0]) ),
+                # terminate if
+                TerminateIf('alleleFreq[0][0]<0.1 or alleleFreq[0][0]>0.9')
+            ],
+            gen = 1000
+        )
+        self.assertEqual(pop.dvars().gen, 1000)
+
+    def testIfElseConstantExpre(self):
+        'Testing operator IfElse with constant expression'
+        def evolveGen(cond):
+            pop = Population(100, loci=[2])
+            return pop.evolve(
+                postOps = IfElse(cond, NoneOp(), TerminateIf("True")),
+                gen = 10)
+        self.assertEqual(evolveGen(''), 1)
+        self.assertEqual(evolveGen([]), 1)
+        self.assertEqual(evolveGen(False), 1)
+        self.assertEqual(evolveGen(True), 10)
+        self.assertEqual(evolveGen('False'), 1)
+        self.assertEqual(evolveGen('True'), 10)
+        self.assertRaises(exceptions.RuntimeError, evolveGen, 'nonsense')
+
+    def testIfElseOperators(self):
+        'Testing opeartor IfElse with multiple operators'
+        simu = Simulator(Population(1000, loci=[2]))
+        # now if we want to flip a lot of alleles whenever it reaches 0.2
+        simu.evolve(
+            # init to 1,2 (or 0, 1 in the binary case)
+            initOps = [ InitSex(), InitGenotype(freq=[.5,.5]) ],
+            matingScheme = RandomMating(),
+            postOps = [
+                # count number of allels at this locus
+                Stat(alleleFreq=[0]),
+                # inject 50% of allele 2 if this allele get low freq
+                IfElse('alleleFreq[0][0]<0.2', ifOps=[
+                    KAlleleMutator(rates=.6, k=2, loci=[0]),
+                    NoneOp(),
+                    ]),
+                # the other way around?
+                IfElse('alleleFreq[0][0]>0.8', ifOps = [
+                    KAlleleMutator(rates=.6, k=2, loci=[0]),
+                    NoneOp()
+                    ]),
+                # terminate if
+                TerminateIf('alleleFreq[0][0]<0.1 or alleleFreq[0][0]>0.9')
+            ],
+            gen = 1000
+        )
+        self.assertEqual(simu.dvars(0).gen, 1000)
+
+
 if __name__ == '__main__':
     unittest.main()
+
+
+
