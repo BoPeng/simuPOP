8bbc90ae6308a928bd299ded5b3ecbad283e9bba
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2010-06-10 02:09:23 +0000

    Add initial files of simuPOP.sandbox

---
+++ trunk/src/sandbox.cpp
@@ -0,0 +1,123 @@
+
+/**
+ *  $File: sandbox.cpp $
+ *  $LastChangedDate: 2010-06-04 13:29:09 -0700 (Fri, 04 Jun 2010) $
+ *  $Rev: 3579 $
+ *
+ *  This file is part of simuPOP, a forward-time population genetics
+ *  simulation environment. Please visit http://simupop.sourceforge.net
+ *  for details.
+ *
+ *  Copyright (C) 2004 - 2010 Bo Peng (bpeng@mdanderson.org)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "sandbox.h"
+
+namespace simuPOP {
+
+
+double InfSitesSelector::indFitness(Individual * ind, ULONG gen) const
+{
+	vectoru chromTypes;
+	const vectoru & loci = m_loci.elems(ind);
+
+	for (size_t i = 0; i < loci.size(); ++i)
+		chromTypes.push_back(ind->chromType(ind->chromLocusPair(loci[i]).first));
+
+	size_t ply = ind->ploidy();
+	if (ind->isHaplodiploid() && ind->sex() == MALE)
+		ply = 1;
+
+	vectori alleles;
+	alleles.reserve(ply * loci.size());
+
+	for (size_t idx = 0; idx < loci.size(); ++idx) {
+		for (size_t p = 0; p < ply; ++p) {
+			if (chromTypes[idx] == CHROMOSOME_Y && ind->sex() == FEMALE)
+				continue;
+			if (((chromTypes[idx] == CHROMOSOME_X && p == 1) ||
+			     (chromTypes[idx] == CHROMOSOME_Y && p == 0)) && ind->sex() == MALE)
+				continue;
+			alleles.push_back(ind->allele(loci[idx], p));
+		}
+	}
+
+	tupleDict::const_iterator pos = m_dict.find(alleles);
+
+	if (pos != m_dict.end())
+		return pos->second;
+
+	if (ply > 1) {
+		// try to look up the key without phase
+		tupleDict::const_iterator it = m_dict.begin();
+		tupleDict::const_iterator itEnd = m_dict.end();
+		for (; it != itEnd; ++it) {
+			bool ok = true;
+			const tupleDict::key_type & key = it->first;
+			UINT begin_idx = 0;
+			UINT end_idx = 0;
+			for (size_t i = 0; i < loci.size(); ++i) {
+				if (chromTypes[i] == CHROMOSOME_Y) {
+					if (ind->sex() == FEMALE)
+						continue;
+					else
+						++end_idx;
+				} else if (chromTypes[i] == CHROMOSOME_X && ind->sex() == MALE)
+					++end_idx;
+				else
+					end_idx += ply;
+				if (key.size() != end_idx - begin_idx) {
+					ok = false;
+					break;
+				}
+				if (ply == 2) {
+					if ((alleles[begin_idx] != key[0] || alleles[end_idx - 1] != key[1]) &&
+					    (alleles[begin_idx] != key[1] || alleles[end_idx - 1] != key[0])) {
+						ok = false;
+						break;
+					}
+				} else {
+					std::sort(alleles.begin() + begin_idx, alleles.begin() + end_idx);
+					tupleDict::key_type sorted_key = it->first;
+					std::sort(sorted_key.begin(), sorted_key.end());
+					for (size_t j = 0; j < sorted_key.size(); ++j) {
+						if (alleles[ply * i + j] != sorted_key[j]) {
+							ok = false;
+							break;
+						}
+					}
+				}
+				begin_idx = end_idx;
+			}
+			if (ok)
+				return it->second;
+		}
+	}
+	// no match
+	string allele_string = "(";
+	for (size_t i = 0; i < alleles.size(); ++i) {
+		if (i != 0)
+			allele_string += ", ";
+		allele_string += toStr(alleles[i]);
+	}
+	allele_string += ")";
+	throw ValueError("No fitness value for genotype " + allele_string);
+	// this line should not be reached.
+	return 0;
+}
+
+
+}
---
+++ trunk/src/sandbox.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+#
+# $File: simuPOP.py $
+# $LastChangedDate: 2010-05-26 11:09:40 -0700 (Wed, 26 May 2010) $
+# $Rev: 3569 $
+#
+# This file is part of simuPOP, a forward-time population genetics
+# simulation environment. Please visit http://simupop.sourceforge.net
+# for details.
+#
+# Copyright (C) 2004 - 2010 Bo Peng (bpeng@mdanderson.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+simuPOP sandbox. This module contains classes (operators and mating schemes)
+and functions that are either experimental (and have a chance to be formally
+added to simuPOP), or designed for special purposes. Because of the temporary
+nature of these classes, it is recommended that modules that use these classes
+include Python version of them so that they can be used when the sandbox
+version is no longer available.
+"""
+
+__all__ = [
+    #'InfSitesMutator',
+    'InfSitesSelector',
+]
+
+# get options
+from simuOpt import simuOptions
+import os, sys
+import exceptions
+
+#
+# These classes are available in binary modules but are not exposed
+# in simuPOP.
+#
+if simuOptions['Optimized']:
+    if simuOptions['AlleleType'] == 'short':
+        from simuPOP_op import *
+    elif simuOptions['AlleleType'] == 'long':
+        from simuPOP_laop import *
+    elif simuOptions['AlleleType'] == 'binary':
+        from simuPOP_baop import *
+    else:
+        from simuPOP_op import *
+else:
+    if simuOptions['AlleleType'] == 'short':
+        from simuPOP_std import *
+    elif simuOptions['AlleleType'] == 'long':
+        from simuPOP_la import *
+    elif simuOptions['AlleleType'] == 'binary':
+        from simuPOP_ba import *
+    else:
+        from simuPOP_std import *
