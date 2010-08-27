9518ef1cfe66b07c19ed5b84552755b6b9385ee1
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2010-08-27 20:02:50 +0000

    Swig generated files has to be under src due to the way packages are generated.

---
+++ 2to3.bat
@@ -0,0 +1,6 @@
+2to3.py -w setup.py
+2to3.py -w simuOpt.py 
+2to3.py -w src\__init__.py src\sampling.py src\plotter.py src\utils.py
+2to3.py -w tools\build.py tools\doxy2swig.py
+2to3.py -w test\*.py
+2to3.py -w doc\userGuide.py doc\runSampleCode.py
