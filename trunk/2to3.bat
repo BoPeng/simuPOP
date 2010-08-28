ad13677aa3c5015355728c13048e88fdd5a104d5
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2010-08-28 04:20:47 +0000

    Fix batch file for 2to3 conversion

--- trunk/2to3.bat
+++ trunk/2to3.bat
@@ -2,5 +2,10 @@
 2to3.py -w simuOpt.py 
 2to3.py -w src\__init__.py src\sampling.py src\plotter.py src\utils.py
 2to3.py -w tools\build.py tools\doxy2swig.py
-2to3.py -w test\*.py
+2to3.py -w test\run_tests.py               test\test_05_matings.py         test\test_11_terminator.py      test\test_17_utils.py
+2to3.py -w test\test_00_genoStru.py        test\test_06_initialization.py  test\test_12_migration.py       test\test_18_plotter.py
+2to3.py -w test\test_01_individual.py      test\test_07_tagging.py         test\test_13_mutation.py        test\test_19_performance.py
+2to3.py -w test\test_02_population.py      test\test_08_stat.py            test\test_14_transmitter.py
+2to3.py -w test\test_03_operator.py        test\test_09_selection.py       test\test_15_penetrance.py
+2to3.py -w test\test_04_simulator.py       test\test_10_qtrait.py          test\test_16_sampling.py
 2to3.py -w doc\userGuide.py doc\runSampleCode.py
