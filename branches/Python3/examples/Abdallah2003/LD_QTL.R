573584376e415b1e810928f008f4f072417a1ca4
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2011-04-13 16:06:30 +0000

    Remove branch Python3

--- branches/Python3/examples/Abdallah2003/LD_QTL.R
+++
@@ -1,5 +0,0 @@
-# deal with pvalues
-pvalues = read.table('pvalues.log', colClasses='numeric', sep=',')
-dim(pvalues)
-p = apply(pvalues, 2 , mean)
-plot(p, main='mean pvalues', xlab='marker distance', ylab='pvalue')
