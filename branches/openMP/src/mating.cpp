8a42eab918ddfcfa2240b925c70001a53cc5683d
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2011-04-21 04:51:29 +0000

    Fix branch

--- branches/openMP/src/mating.cpp
+++ branches/openMP/src/mating.cpp
@@ -1314,17 +1314,17 @@ bool HomoMating::mateSubPop(Population & pop, Population & offPop, size_t subPop
 		// in this case, openMP must have been supported with numThreads() > 1
 #ifdef _OPENMP
 		size_t offPopSize = offEnd - offBegin;
-		UINT numOffspring = m_OffspringGenerator->numOffspring(pop.gen());
-		int nThreads = numThreads();
+		ssize_t blockSize = m_OffspringGenerator->numOffspring(pop.gen()) * 
+			// (offPopSize / 100);   // control the number of blocks?
+			100;					// control size of blocks?
 		int except = 0;
-		int block  = offPopSize/numOffspring/(nThreads*2);
 		string msg;
 #  pragma omp parallel for 
-		for(int i=0; i <= static_cast<int>(offPopSize/numOffspring/block); i++)
+		for(int i=0; i <= static_cast<int>(offPopSize/blockSize); i++)
 		{
 		  try {
-				RawIndIterator local_it = offBegin + i*numOffspring;
-				RawIndIterator local_offEnd = offEnd - local_it > numOffspring ? local_it + numOffspring : offEnd;
+				RawIndIterator local_it = offBegin + i*blockSize;
+				RawIndIterator local_offEnd = offEnd - local_it > blockSize ? local_it + blockSize : offEnd;
 
 				while (local_it != local_offEnd) {
 					if (except)
