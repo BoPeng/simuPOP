405a7d0d620670530607bb3c34862479df28724f
simupop <simupop@4f9f4801-c785-45ef-b63d-f11ee613d617>
2012-10-04 14:21:10 +0000

    Fix LDNe test for binary modules

--- test/test_08_stat.py
+++ test/test_08_stat.py
@@ -823,23 +823,25 @@ class TestStat(unittest.TestCase):
         #turnOnDebug('DBG_STATOR')
         #turnOnDebug('DBG_DEVEL')
         stat(pop, effectiveSize=ALL_AVAIL, vars=['Ne_LD', 'Ne_LD_mono'])
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][0], 137.6, 1)
-        # parametric estimates 
-        #self.assertAlmostEqual(pop.dvars().Ne_LD[1], 74.8, 1)
-        #self.assertAlmostEqual(pop.dvars().Ne_LD[2], 413.4, 1)
-        # Jackknife
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][1], 76.4, 0)
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][2], 383.9, 0)
+        if moduleInfo()['alleleType'] != 'binary':
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][0], 137.6, 1)
+            # parametric estimates 
+            #self.assertAlmostEqual(pop.dvars().Ne_LD[1], 74.8, 1)
+            #self.assertAlmostEqual(pop.dvars().Ne_LD[2], 413.4, 1)
+            # Jackknife
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][1], 76.4, 0)
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][2], 383.9, 0)
+            #
+            # monopoly
+            self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][0], 276.7, 1)
+            # parametric estimate
+            #self.assertAlmostEqual(pop.dvars().Ne_LD_mono[1], 151.1, 1)
+            #self.assertAlmostEqual(pop.dvars().Ne_LD_mono[2], 828.3, 0)
+            # jackknife
+            self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][1], 154.4, 1)
+            self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][2], 769.2, 0)
         #
-        # monopoly
-        self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][0], 276.7, 1)
-        # parametric estimate
-        #self.assertAlmostEqual(pop.dvars().Ne_LD_mono[1], 151.1, 1)
-        #self.assertAlmostEqual(pop.dvars().Ne_LD_mono[2], 828.3, 0)
-        # jackknife
-        self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][1], 154.4, 1)
-        self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][2], 769.2, 0)
-        #
+        # calculate LD Ne
         #
         # calculate LD Ne
         #turnOnDebug('DBG_STATOR')
@@ -852,28 +854,29 @@ class TestStat(unittest.TestCase):
             matingScheme=RandomMating(),
             gen=10
         )
-        export(pop, output='ldnenew.txt', format='GENEPOP', gui=False)
+        #export(pop, output='ldnenew.txt', format='GENEPOP', gui=False)
         #turnOnDebug('DBG_STATOR')
         #turnOnDebug('DBG_DEVEL')
         #turnOnDebug('DBG_WARNING')
         stat(pop, effectiveSize=range(10), vars=['Ne_LD', 'Ne_LD_mono'])
         #
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.05][0], 495.3, 1)
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.05][1], 377.9, 0)
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.05][2], 685.7, 0)
-        #
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.02][0], 464.9, 1)
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.02][1], 397.3, 0)
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.02][2], 552.8, 0)
-        #
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][0], 495.1, 1)
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][1], 423.0, 0)
-        self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][2], 589.5, 0)
-        # monopoly
-        self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][0], 991.7, 1)
-        self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][1], 847.5, 1)
-        self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][2], 1180.4, 0)
-        #
+        if moduleInfo()['alleleType'] != 'binary':
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.05][0], 495.3, 1)
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.05][1], 377.9, 0)
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.05][2], 685.7, 0)
+            #
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.02][0], 464.9, 1)
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.02][1], 397.3, 0)
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.02][2], 552.8, 0)
+            #
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][0], 495.1, 1)
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][1], 423.0, 0)
+            self.assertAlmostEqual(pop.dvars().Ne_LD[0.01][2], 589.5, 0)
+            # monopoly
+            self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][0], 991.7, 1)
+            self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][1], 847.5, 1)
+            self.assertAlmostEqual(pop.dvars().Ne_LD_mono[0.01][2], 1180.4, 0)
+            #
         #  do not crash for fixed loci (will return nan, inf etc)
         pop = Population(size=[500], loci=[1]*10)
         stat(pop, effectiveSize=range(10), vars='Ne_LD')
