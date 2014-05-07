22ef090557951606e66531d16fd1bb5d91f8d023
bpeng2000 <bpeng2000@4f9f4801-c785-45ef-b63d-f11ee613d617>
2014-05-07 14:57:53 +0000

    Test for float population size returned by a demographic function.

--- trunk/src/utility.cpp
+++ trunk/src/utility.cpp
@@ -80,7 +80,6 @@ using std::ofstream;
 #  define PyInt_AS_LONG(x) PyLong_AsLong(x)
 #  define PyInt_FromLong(x) PyLong_FromLong(x)
 #  define PyInt_FromSize_t(x) PyLong_FromSize_t(x)
-#  define PyNumber_Int(x) PyNumber_Long(x)
 #  define PyInt_FromString PyLong_FromString
 #  define PyInt_Type PyLong_Type
 #  define PyFloat_FromString(x, y)  PyFloat_FromString(x)
@@ -1138,7 +1137,7 @@ void PyObj_As_Int(PyObject * obj, long & val)
 		return;
 	}
 	// try to convert
-	PyObject * res = PyNumber_Int(obj);
+	PyObject * res = PyNumber_Long(obj);
 	if (res == NULL)
 		throw ValueError("Can not convert object to an integer");
 
