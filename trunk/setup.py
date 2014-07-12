bfbfddf5beb1ceee9437d8d697c08ae560eb8698
bpeng2000 <bpeng2000@4f9f4801-c785-45ef-b63d-f11ee613d617>
2014-07-12 21:00:45 +0000

    Revert part of the previous patch

--- trunk/setup.py
+++ trunk/setup.py
@@ -741,7 +741,8 @@ if os.name == 'nt':    # Windows
 if __name__ == '__main__':
     SIMUPOP_VER, SIMUPOP_REV = simuPOP_version()
     # create source file for each module
-    MODULES = ['std']
+    MODULES = ['std', 'op', 'la', 'laop', 'ba', 'baop', 'mu', 'muop', 'lin', 'linop']
+     
     #
     # Generate Wrapping files
     #
